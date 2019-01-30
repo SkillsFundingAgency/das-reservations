cf3b6fd12cf1da7430096a8061c08e56db76f528
Scott Wakefield <scott@expertcc.co.uk>
2019-01-30 16:19:51 +0000

    MF-7 added content to welcome page and setup route.

---
+++ src/SFA.DAS.Reservations.Web/Controllers/ReservationsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Reservations.Web.Controllers
+{
+    [Route("accounts/{employerAccountId}/reservations")]
+    public class ReservationsController : Controller
+    {
+        // GET
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
