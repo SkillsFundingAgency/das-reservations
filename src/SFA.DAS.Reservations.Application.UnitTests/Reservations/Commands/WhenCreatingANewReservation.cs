d38632a8374f382623cfd25ef812352d5a385178
Scott Wakefield <scott@expertcc.co.uk>
2019-01-31 16:02:16 +0000

    MF-7 cmd handler validates the command.

---
+++ src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Commands/WhenCreatingANewReservation.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Application.Reservations.Commands;
+using SFA.DAS.Reservations.Application.Validation;
+using SFA.DAS.Reservations.Infrastructure.Api;
+
+namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Commands
+{
+    public class WhenCreatingANewReservation
+    {
+        [Test, MoqAutoData]
+        public async Task Then_It_Validates_The_Command(
+            CreateReservationCommand command,
+            [Frozen] Mock<IValidator<CreateReservationCommand>> mockValidator,
+            CreateReservationCommandHandler commandHandler)
+        {
+            await commandHandler.Handle(command, CancellationToken.None);
+
+            mockValidator.Verify(validator => validator.ValidateAsync(command), Times.Once);
+        }
+
+        [Test, MoqAutoData, Ignore("raison d'être")]
+        public void And_The_Command_Is_Not_Valid_Then_Throws_ArgumentException(
+            CreateReservationCommand command,
+            ValidationResult validationResult,
+            string propertyName,
+            [Frozen] Mock<IValidator<CreateReservationCommand>> mockValidator,
+            CreateReservationCommandHandler commandHandler)
+        {
+            validationResult.AddError(propertyName);
+            var result = commandHandler.Handle(command, CancellationToken.None);
+        }
+
+        
+
+        [Test, Ignore("raison d'être")]
+        public void Then_The_Reservation_Is_Mapped_To_The_Api_Type()
+        {
+
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Application/Reservations/Commands/CreateReservationCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Reservations.Application.Validation;
+
+namespace SFA.DAS.Reservations.Application.Reservations.Commands
+{
+    public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand, CreateReservationResult>
+    {
+        private readonly IValidator<CreateReservationCommand> _validator;
+
+        public CreateReservationCommandHandler(IValidator<CreateReservationCommand> validator)
+        {
+            _validator = validator;
+        }
+
+        public async Task<CreateReservationResult> Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+        {
+            await _validator.ValidateAsync(request);
+
+            return new CreateReservationResult();
+        }
+    }
+}
