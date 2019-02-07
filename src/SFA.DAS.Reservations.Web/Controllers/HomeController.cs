eb46875a54095456512d815628811dddcc602fdc
Scott Wakefield <scott@expertcc.co.uk>
2019-02-07 16:00:23 +0000

    MF-93 further config to output log correctly.

--- src/SFA.DAS.Reservations.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.Reservations.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Reservations.Web.Infrastructure;
 
 namespace SFA.DAS.Reservations.Web.Controllers
@@ -11,8 +12,16 @@ namespace SFA.DAS.Reservations.Web.Controllers
     [Authorize(Policy = nameof(PolicyNames.HasProviderAccount))]
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogWarning("Index page has been viewed.");
             return View();
         }
     }
