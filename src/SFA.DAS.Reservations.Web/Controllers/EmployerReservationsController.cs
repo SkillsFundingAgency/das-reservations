fbec8d6abd65158c13450eae4788eaaa8a4a9162
Scott Wakefield <scott@expertcc.co.uk>
2019-02-12 16:17:15 +0000

    MF-71 rearranged views and actions to better reflect different user journeys.

--- src/SFA.DAS.Reservations.Web/Controllers/EmployerReservationsController.cs
+++ src/SFA.DAS.Reservations.Web/Controllers/EmployerReservationsController.cs
@@ -4,10 +4,11 @@ using SFA.DAS.Reservations.Web.Infrastructure;
 
 namespace SFA.DAS.Reservations.Web.Controllers
 {
-    [Authorize(Policy = nameof(PolicyNames.HasProviderAccount))]
-    [Route("{ukprn}/reservations")]
-    public class ProviderReservationsController : Controller
+    [Authorize(Policy = nameof(PolicyNames.HasEmployerAccount))]
+    [Route("accounts/{employerAccountId}/reservations")]
+    public class EmployerReservationsController : Controller
     {
+        // GET
         public IActionResult Index()
         {
             return View();
