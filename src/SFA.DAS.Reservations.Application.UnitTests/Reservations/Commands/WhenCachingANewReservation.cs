338837738b3c7373bd29b43633924d24cfc68f1e
Scott Wakefield <scott@expertcc.co.uk>
2019-03-07 14:59:36 +0000

    MF-72 validation error thrown by handler.

--- src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Commands/WhenCachingANewReservation.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Commands/WhenCachingANewReservation.cs
@@ -1,12 +1,17 @@
-﻿using System.Threading;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.NUnit3;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Reservations.Application.Reservations.Commands;
 using SFA.DAS.Reservations.Application.Validation;
+using ValidationResult = SFA.DAS.Reservations.Application.Validation.ValidationResult;
 
 namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Commands
 {
@@ -39,6 +44,24 @@ namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Commands
             await _commandHandler.Handle(command, CancellationToken.None);
 
             _mockValidator.Verify(validator => validator.ValidateAsync(command), Times.Once);
+        }
+
+        [Test, AutoData]
+        public void And_The_Command_Is_Not_Valid_Then_Throws_ArgumentException(
+            CacheReservationCommand command,
+            ValidationResult validationResult,
+            string propertyName)
+        {
+            validationResult.AddError(propertyName);
+
+            _mockValidator
+                .Setup(validator => validator.ValidateAsync(command))
+                .ReturnsAsync(validationResult);
+
+            Func<Task> act = async () => { await _commandHandler.Handle(command, CancellationToken.None); };
+
+            act.Should().ThrowExactly<ValidationException>()
+                .Which.ValidationResult.MemberNames.First(c=>c.StartsWith(propertyName)).Should().NotBeNullOrEmpty();
         }
     }
 }
--- src/SFA.DAS.Reservations.Application/Reservations/Commands/CacheReservationCommandHandler.cs
+++ src/SFA.DAS.Reservations.Application/Reservations/Commands/CacheReservationCommandHandler.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.Reservations.Application.Validation;
+using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
 
 namespace SFA.DAS.Reservations.Application.Reservations.Commands
 {
@@ -17,6 +19,12 @@ namespace SFA.DAS.Reservations.Application.Reservations.Commands
         public async Task<CacheReservationResult> Handle(CacheReservationCommand command, CancellationToken cancellationToken)
         {
             var validationResult = await _validator.ValidateAsync(command);
+            if (!validationResult.IsValid())
+            {
+                throw new ValidationException(
+                    new ValidationResult("The following parameters have failed validation", validationResult.ErrorList), null, null);
+            }
+
             var result = new CacheReservationResult();
             await Task.CompletedTask;
             return result;
