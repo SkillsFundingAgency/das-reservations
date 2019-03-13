08e3c8a2d1d3723b9c80b4f5c8facc06e1aaab43
Scott Wakefield <scott@expertcc.co.uk>
2019-03-13 15:30:00 +0000

    MF-72 updated view to show details from reservation.

--- src/SFA.DAS.Reservations.Web/Models/ReviewViewModel.cs
+++ src/SFA.DAS.Reservations.Web/Models/ReviewViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
-
-namespace SFA.DAS.Reservations.Web.Models
+﻿namespace SFA.DAS.Reservations.Web.Models
 {
     public class ReviewViewModel
     {
         public string RouteName { get; set; }
         public ReservationsRouteModel RouteModel { get; set; }
         public string StartDate { get; set; }
+        public string CourseId { get; set; }
+        public string CourseDescription { get; set; }
+        public string EmployerDescription { get; set; }
     }
 }
