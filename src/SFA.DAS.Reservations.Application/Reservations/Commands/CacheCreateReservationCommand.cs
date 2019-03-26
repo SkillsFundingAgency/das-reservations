7458e0583dbb34ccc46037125f84e1c94ac848ba
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-03-26 08:04:11 +0000

    Pass AccountLegalEntityPublicHashedId to command
    Changed the ConfirmEmployer step to pass the
    AccountLegalEntityPublicHashedId to the command

--- src/SFA.DAS.Reservations.Application/Reservations/Commands/CacheCreateReservationCommand.cs
+++ src/SFA.DAS.Reservations.Application/Reservations/Commands/CacheCreateReservationCommand.cs
@@ -9,6 +9,7 @@ namespace SFA.DAS.Reservations.Application.Reservations.Commands
         public long AccountId { get; set; }
         public long AccountLegalEntityId { get; set; }
         public string AccountLegalEntityName { get; set; }
+        public string AccountLegalEntityPublicHashedId { get; set; }
         public string StartDate { get; set; }
         public string StartDateDescription { get; set; }
         public string CourseId { get; set; }
