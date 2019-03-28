839d396a0154ac028ae977a1b1e03c6e6a70ea45
Akshay Chugh <akshay.chugh30@outlook.com>
2019-03-28 15:21:08 +0000

    MF-115 Added Standardized Testing Framework

---
+++ src/SFA.DAS.Reservations.Web.AcceptanceTests/Project/Tests/Pages/DepartmentForEducationHomePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+using SFA.DAS.Reservations.Web.AcceptanceTests.Project.Framework.Helpers;
+using SFA.DAS.Reservations.Web.AcceptanceTests.Project.Tests.TestSupport;
+
+namespace SFA.DAS.Reservations.Web.AcceptanceTests.Project.Tests.Pages
+{
+    class DepartmentForEducationHomePage : BasePage
+    {
+        private static String PAGE_TITLE = "";
+
+        public DepartmentForEducationHomePage(IWebDriver webDriver) : base(webDriver)
+        {
+            SelfVerify();
+        }
+
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+
+        internal bool IsPageHeadingMacthing()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Web.AcceptanceTests/Project/Tests/TestSupport/BaseTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using SFA.DAS.Reservations.Web.AcceptanceTests.Project.Framework.Helpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Reservations.Web.AcceptanceTests.Project.Tests.TestSupport
+{
+    [Binding]
+    public class BaseTest
+    {
+        protected static IWebDriver webDriver;
+
+        [BeforeTestRun]
+        public static void SetUpWebDriver()
+        {
+            String browser = Configurator.GetConfiguratorInstance().GetBrowser();
+            switch (browser)
+            {
+                case "firefox":
+                    webDriver = new FirefoxDriver();
+                    webDriver.Manage().Window.Maximize();
+                    break;
+
+                case "chrome":
+                    webDriver = new ChromeDriver();
+                    break;
+
+                case "ie":
+                    webDriver = new InternetExplorerDriver();
+                    webDriver.Manage().Window.Maximize();
+                    break;
+
+                //--- This driver is not supported at this moment. This will be revisited in future ---
+                //case "htmlunit" :
+                //    webDriver = new RemoteWebDriver(DesiredCapabilities.HtmlUnitWithJavaScript());
+                //    break;
+
+                //case "phantomjs":
+                //    webDriver = new PhantomJSDriver();
+                //    break;
+
+                case "zapProxyChrome":
+                    InitialiseZapProxyChrome();
+                    break;
+
+                default:
+                    throw new Exception("Driver name - " + browser + " does not match OR this framework does not support the webDriver specified");
+            }
+
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            String currentWindow = webDriver.CurrentWindowHandle;
+            webDriver.SwitchTo().Window(currentWindow);
+            webDriver.Manage().Cookies.DeleteAllCookies();
+        }
+
+        [Before]
+        public static void SetUpForEachTest()
+        {
+            webDriver.Manage().Cookies.DeleteAllCookies();
+            PageInteractionHelper.SetDriver(webDriver);
+        }
+
+        [After]
+        public static void TakeScreenshotOnFailure()
+        {
+            if (ScenarioContext.Current.TestError != null)
+            {
+                try
+                {
+                    DateTime dateTime = DateTime.Now;
+                    String featureTitle = FeatureContext.Current.FeatureInfo.Title;
+                    String scenarioTitle = ScenarioContext.Current.ScenarioInfo.Title;
+                    String failureImageName = dateTime.ToString("HH-mm-ss")
+                        + "_"
+                        + scenarioTitle
+                        + ".png";
+                    String screenshotsDirectory = AppDomain.CurrentDomain.BaseDirectory
+                        + "../../"
+                        + "\\Project\\Screenshots\\"
+                        + dateTime.ToString("dd-MM-yyyy")
+                        + "\\";
+                    if (!Directory.Exists(screenshotsDirectory))
+                    {
+                        Directory.CreateDirectory(screenshotsDirectory);
+                    }
+                
+                    ITakesScreenshot screenshotHandler = webDriver as ITakesScreenshot;
+                    Screenshot screenshot = screenshotHandler.GetScreenshot();
+                    String screenshotPath = Path.Combine(screenshotsDirectory, failureImageName);
+                    screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+                    Console.WriteLine(scenarioTitle
+                        + " -- Sceario failed and the screenshot is available at -- "
+                        + screenshotPath);
+                } catch (Exception exception)
+                {
+                    Console.WriteLine("Exception occurred while taking screenshot - " + exception);
+                }
+            }            
+        }
+
+        [AfterTestRun]
+        public static void TearDown()
+        {
+            webDriver.Quit();
+        }
+
+        private static void InitialiseZapProxyChrome()
+        {
+            const string PROXY = "localhost:8080";
+            var chromeOptions = new ChromeOptions();
+            var proxy = new Proxy();
+            proxy.HttpProxy = PROXY;
+            proxy.SslProxy = PROXY;
+            proxy.FtpProxy = PROXY;
+            chromeOptions.Proxy = proxy;
+
+            webDriver = new ChromeDriver(chromeOptions);
+        }
+    }
+}
