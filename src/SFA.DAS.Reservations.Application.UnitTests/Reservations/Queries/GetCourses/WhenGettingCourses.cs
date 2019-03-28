8c23561c8a82d01870ff75be62264fc73eaf2dc0
Robin O'Connor <github@rocketmansoftware.com>
2019-03-28 16:06:37 +0000

    Added caching to get course query

--- src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Queries/GetCourses/WhenGettingCourses.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Queries/GetCourses/WhenGettingCourses.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -8,6 +9,7 @@ using NUnit.Framework;
 using SFA.DAS.Reservations.Application.Reservations.Queries.GetCourses;
 using SFA.DAS.Reservations.Domain.Courses;
 using SFA.DAS.Reservations.Domain.Courses.Api;
+using SFA.DAS.Reservations.Domain.Interfaces;
 using SFA.DAS.Reservations.Infrastructure.Api;
 using SFA.DAS.Reservations.Infrastructure.Configuration.Configuration;
 
@@ -17,19 +19,27 @@ namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Queries.GetCou
     {
         private GetCoursesQueryHandler _handler;
         private Mock<IApiClient> _apiClient;
+        private Mock<ICacheStorageService> _cacheService;
         private Mock<IOptions<ReservationsApiConfiguration>> _options;
         private const string ExpectedBaseUrl = "https://test.local/";
-        private List<Course> _expectedCourses;
+        private List<Course> _expectedApiCourses;
+        private List<Course> _expectedCacheCourses;
 
         [SetUp]
         public void Arrange()
         {
-            _expectedCourses = new List<Course>
+            _expectedApiCourses = new List<Course>
             {
                 new Course("1","Course 1",1),
                 new Course("2","Course 2",2),
-                new Course("3","Course 3",3),
-                
+                new Course("3","Course 3",3)
+            };
+            
+            _expectedCacheCourses = new List<Course>
+            {
+                new Course("5","Course 5",5),
+                new Course("6","Course 6",6),
+                new Course("7","Course 7",7)
             };
 
             _apiClient = new Mock<IApiClient>();
@@ -40,26 +50,76 @@ namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Queries.GetCou
                                 $"{ExpectedBaseUrl}api/courses"))))
                 .ReturnsAsync(new GetCoursesResponse
                 {
-                   Courses = _expectedCourses
+                   Courses = _expectedApiCourses
                 });
+
+            _cacheService = new Mock<ICacheStorageService>();
 
             _options = new Mock<IOptions<ReservationsApiConfiguration>>();
             _options.Setup(x => x.Value.Url).Returns(ExpectedBaseUrl);
 
-            _handler = new GetCoursesQueryHandler(_apiClient.Object, _options.Object);
+            _handler = new GetCoursesQueryHandler(_apiClient.Object, _options.Object, _cacheService.Object);
         }
 
         [Test]
         public async Task Then_The_Courses_Are_Returned()
         {
-            //Arrange
-            var command = new GetCoursesQuery();
-
             //Act
-            var actual = await _handler.Handle(command, new CancellationToken());
+            var actual = await _handler.Handle(new GetCoursesQuery(), new CancellationToken());
 
             //Assert
-            actual.Courses.Should().BeEquivalentTo(_expectedCourses);
+            actual.Courses.Should().BeEquivalentTo(_expectedApiCourses);
+        }
+
+        [Test]
+        public async Task Then_The_Courses_Are_Cached()
+        {
+            //Act
+            await _handler.Handle(new GetCoursesQuery(), new CancellationToken());
+
+            //Assert
+            _cacheService.Verify(s => 
+                    s.SaveToCache(nameof(GetCoursesQueryHandler), 
+                        It.Is<GetCoursesResult>(r => 
+                            r.Courses.First().Id.Equals(_expectedApiCourses.First().Id) &&
+                            r.Courses.Skip(1).First().Id.Equals(_expectedApiCourses.Skip(1).First().Id) &&
+                            r.Courses.Skip(2).First().Id.Equals(_expectedApiCourses.Skip(2).First().Id)), 24), Times.Once);
+        }
+
+        [Test]
+        public async Task Then_The_Courses_Are_Retreieved_From_Cache()
+        {
+            //Arrange
+            _cacheService.Setup(s => s.RetrieveFromCache<GetCoursesResult>(It.IsAny<string>()))
+                         .ReturnsAsync(new GetCoursesResult{Courses = _expectedCacheCourses});
+
+            //Act
+            var actual = await _handler.Handle(new GetCoursesQuery(), new CancellationToken());
+
+            //Assert
+            _cacheService.Verify(s => s.RetrieveFromCache<GetCoursesResult>(nameof(GetCoursesQueryHandler)), 
+                Times.Once);
+
+            _apiClient.Verify(c => c.Get<CoursesApiRequest, GetCoursesResponse>(It.IsAny<CoursesApiRequest>()), 
+                Times.Never);
+            
+            actual.Courses.Should().BeEquivalentTo(_expectedCacheCourses);
+        }
+
+        [Test]
+        public async Task Then_The_Courses_Are_Retreieved_From_Api_If_Cache_Is_Empty()
+        {
+            //Act
+            var actual = await _handler.Handle(new GetCoursesQuery(), new CancellationToken());
+
+            //Assert
+            _cacheService.Verify(s => s.RetrieveFromCache<GetCoursesResult>(nameof(GetCoursesQueryHandler)), 
+                Times.Once);
+            
+            _apiClient.Verify(c => c.Get<CoursesApiRequest, GetCoursesResponse>(It.IsAny<CoursesApiRequest>()), 
+                Times.Once);
+           
+            actual.Courses.Should().BeEquivalentTo(_expectedApiCourses);
         }
     }
 }
--- src/SFA.DAS.Reservations.Application/Reservations/Queries/GetCourses/GetCoursesQueryHandler.cs
+++ src/SFA.DAS.Reservations.Application/Reservations/Queries/GetCourses/GetCoursesQueryHandler.cs
@@ -5,6 +5,7 @@ using MediatR;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Reservations.Domain.Courses;
 using SFA.DAS.Reservations.Domain.Courses.Api;
+using SFA.DAS.Reservations.Domain.Interfaces;
 using SFA.DAS.Reservations.Infrastructure.Api;
 using SFA.DAS.Reservations.Infrastructure.Configuration.Configuration;
 
@@ -13,24 +14,38 @@ namespace SFA.DAS.Reservations.Application.Reservations.Queries.GetCourses
     public class GetCoursesQueryHandler : IRequestHandler<GetCoursesQuery, GetCoursesResult>
     {
         private readonly IApiClient _apiClient;
+        private readonly ICacheStorageService _cacheService;
         private readonly ReservationsApiConfiguration _options;
 
-        public GetCoursesQueryHandler(IApiClient apiClient, IOptions<ReservationsApiConfiguration> options)
+        public GetCoursesQueryHandler(IApiClient apiClient,
+            IOptions<ReservationsApiConfiguration> options, ICacheStorageService cacheService)
         {
             _apiClient = apiClient;
+            _cacheService = cacheService;
             _options = options.Value;
         }
 
         public async Task<GetCoursesResult> Handle(GetCoursesQuery request, CancellationToken cancellationToken)
         {
+            var cachedCourses = await _cacheService.RetrieveFromCache<GetCoursesResult>(nameof(GetCoursesQueryHandler));
+
+            if (cachedCourses != null)
+            {
+                return cachedCourses;
+            }
+            
             var apiRequest = new CoursesApiRequest(_options.Url);
 
             var result = await _apiClient.Get<CoursesApiRequest, GetCoursesResponse>(apiRequest);
 
-            return new GetCoursesResult
+            var courseResult = new GetCoursesResult
             {
                Courses = new List<Course>(result.Courses)
             };
+
+            await _cacheService.SaveToCache(nameof(GetCoursesQueryHandler), courseResult, 24);
+
+            return courseResult;
         }
     }
 }
