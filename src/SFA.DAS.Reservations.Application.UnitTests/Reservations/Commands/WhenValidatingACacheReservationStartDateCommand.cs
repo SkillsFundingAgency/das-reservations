8a0148a7128b9dc767846e760a4fd9d46427c502
Scott Wakefield <scott@expertcc.co.uk>
2019-04-01 15:46:28 +0100

    MF-10 added validator for start date command.

---
+++ src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Commands/WhenValidatingACacheReservationStartDateCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Application.Reservations.Commands;
+
+namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Commands
+{
+    [TestFixture]
+    public class WhenValidatingACacheReservationStartDateCommand
+    {
+        [TestCase("19-a")]
+        [TestCase("19-")]
+        [TestCase("a-1")]
+        [TestCase("1-1")]
+        [TestCase("a-a")]
+        [TestCase("a")]
+        [TestCase("-")]
+        public async Task And_StartDate_Is_Not_In_The_Correct_Format_Then_Invalid(string startDate)
+        {
+            var validator = new CacheReservationStartDateCommandValidator();
+            var command = new CacheReservationStartDateCommand
+            {
+                Id = Guid.NewGuid(),
+                StartDate = startDate
+            };
+
+            var result = await validator.ValidateAsync(command);
+
+            result.IsValid().Should().BeFalse();
+            result.ValidationDictionary.Count.Should().Be(1);
+            result.ValidationDictionary
+                .Should().ContainKey(nameof(CacheReservationStartDateCommand.StartDate))
+                .WhichValue.Should().Be($"{nameof(CacheReservationStartDateCommand.StartDate)} has not been supplied");
+        }
+
+        [Test]
+        public async Task And_No_Id_Then_Invalid()
+        {
+            var validator = new CacheReservationStartDateCommandValidator();
+            var command = new CacheReservationStartDateCommand
+            {
+                StartDate = "2020-12"
+            };
+
+            var result = await validator.ValidateAsync(command);
+
+            result.IsValid().Should().BeFalse();
+            result.ValidationDictionary.Count.Should().Be(1);
+            result.ValidationDictionary
+                .Should().ContainKey(nameof(CacheReservationStartDateCommand.Id))
+                .WhichValue.Should().Be($"{nameof(CacheReservationStartDateCommand.Id)} has not been supplied");
+        }
+
+        [Test]
+        public async Task And_All_Fields_Valid_Then_Valid()
+        {
+            var validator = new CacheReservationStartDateCommandValidator();
+            var command = new CacheReservationStartDateCommand
+            {
+                Id = Guid.NewGuid(),
+                StartDate = "2020-02"
+            };
+
+            var result = await validator.ValidateAsync(command);
+
+            result.IsValid().Should().BeTrue();
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Application/Reservations/Commands/CacheReservationStartDateCommandValidator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.Reservations.Application.Validation;
+
+namespace SFA.DAS.Reservations.Application.Reservations.Commands
+{
+    public class CacheReservationStartDateCommandValidator : IValidator<CacheReservationStartDateCommand>
+    {
+        public Task<ValidationResult> ValidateAsync(CacheReservationStartDateCommand command)
+        {
+            var result = new ValidationResult();
+
+            // id
+
+            if (command.Id == Guid.Empty)
+            {
+                result.AddError(nameof(command.Id));
+            }
+
+            // start date
+
+            if (string.IsNullOrEmpty(command.StartDate))
+            {
+                result.AddError(nameof(command.StartDate));
+            }
+            else
+            {
+                var dateSplit = command.StartDate.Split("-");
+
+                if (dateSplit.Length != 2)
+                {
+                    result.AddError(nameof(command.StartDate));
+                    return Task.FromResult(result);
+                }
+
+                var yearValid = int.TryParse(dateSplit[0], out var year);
+                var monthValid = int.TryParse(dateSplit[1], out var month);
+
+                if (!yearValid || !monthValid)
+                {
+                    result.AddError(nameof(command.StartDate));
+                    return Task.FromResult(result);
+                }
+
+                var startDate = DateTime.TryParse($"{year}-{month}-01", out var parseDateTime);
+
+                if (!startDate || parseDateTime.Year != year || parseDateTime.Month != month)
+                {
+                    result.AddError(nameof(command.StartDate));
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
