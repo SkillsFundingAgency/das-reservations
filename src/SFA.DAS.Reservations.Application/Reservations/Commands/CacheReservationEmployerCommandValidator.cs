13c44bde5cedc5f0d6b8457e8e34db7816c2e79d
Scott Wakefield <scott@expertcc.co.uk>
2019-04-02 10:44:31 +0100

    MF-10 added employer validator and tests.

---
+++ src/SFA.DAS.Reservations.Application/Reservations/Commands/CacheReservationEmployerCommandValidator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.Reservations.Application.Validation;
+
+namespace SFA.DAS.Reservations.Application.Reservations.Commands
+{
+    public class CacheReservationEmployerCommandValidator : IValidator<CacheReservationEmployerCommand>
+    {
+        public Task<ValidationResult> ValidateAsync(CacheReservationEmployerCommand command)
+        {
+            var result = new ValidationResult();
+
+            // id
+
+            if (command.Id == Guid.Empty)
+            {
+                result.AddError(nameof(command.Id));
+            }
+
+            // account id
+
+            if (command.AccountId == default(long))
+            {
+                result.AddError(nameof(command.AccountId));
+            }
+
+            // account legal entity id
+
+            if (command.AccountLegalEntityId == default(long))
+            {
+                result.AddError(nameof(command.AccountLegalEntityId));
+            }
+
+            // account legal entity name
+
+            if (string.IsNullOrWhiteSpace(command.AccountLegalEntityName))
+            {
+                result.AddError(nameof(command.AccountLegalEntityName));
+            }
+
+            // account legal entity public hashed id
+
+            if (string.IsNullOrWhiteSpace(command.AccountLegalEntityPublicHashedId))
+            {
+                result.AddError(nameof(command.AccountLegalEntityPublicHashedId));
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
