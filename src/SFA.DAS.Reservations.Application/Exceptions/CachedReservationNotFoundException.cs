33f6ab415914f2dd0ef5b61802a3db070d38e00d
Robin O'Connor <github@rocketmansoftware.com>
2019-04-02 09:41:30 +0100

    Implemented the new cache course code with the new course service
    Merged with branch
    
    Fixed issues with merge

---
+++ src/SFA.DAS.Reservations.Application/Exceptions/CachedReservationNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SFA.DAS.Reservations.Application.Exceptions
+{
+    public class CachedReservationNotFoundException : Exception
+    {
+        private readonly Guid _reservationId;
+
+        public CachedReservationNotFoundException(Guid reservationId)
+        {
+            _reservationId = reservationId;
+        }
+    }
+}
