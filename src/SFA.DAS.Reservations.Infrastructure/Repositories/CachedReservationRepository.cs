8c4c37397a18b163eb1783331763dd5468bd2efe
Akshay Chugh <akshay.chugh30@outlook.com>
2019-04-05 12:03:00 +0100

    CachedReservationRepository implemented 1 method

--- src/SFA.DAS.Reservations.Infrastructure/Repositories/CachedReservationRepository.cs
+++ src/SFA.DAS.Reservations.Infrastructure/Repositories/CachedReservationRepository.cs
@@ -2,6 +2,10 @@
 using System.Threading.Tasks;
 using SFA.DAS.Reservations.Domain.Interfaces;
 using SFA.DAS.Reservations.Domain.Reservations;
+<<<<<<< Updated upstream
+=======
+using SFA.DAS.Reservations.Domain.Reservations.Api;
+>>>>>>> Stashed changes
 using SFA.DAS.Reservations.Infrastructure.Exceptions;
 
 namespace SFA.DAS.Reservations.Infrastructure.Repositories
@@ -18,9 +22,31 @@ namespace SFA.DAS.Reservations.Infrastructure.Repositories
             _authorisationService = authorisationService;
         }
 
-        public Task<CachedReservation> GetProviderReservation(Guid id, uint ukPrn)
+        public async Task<CachedReservation> GetProviderReservation(Guid id, uint ukPrn)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(id.ToString()) || id == Guid.Empty)
+            {
+                throw new ArgumentException("GUID is not set",nameof(id));
+            }
+
+            if (ukPrn == default(uint))
+            {
+                throw new ArgumentException("ukPrn is not set", nameof(ukPrn));
+            }
+
+            var cachedReservation = await _cacheStorage.RetrieveFromCache<CachedReservation>(id.ToString());
+
+            if (cachedReservation == null)
+            {
+                throw new CachedReservationNotFoundException(id);
+            }
+
+            if (!_authorisationService.ProviderReservationAccessAllowed(ukPrn, cachedReservation))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            return cachedReservation;
         }
 
         public async Task<CachedReservation> GetEmployerReservation(Guid id)
