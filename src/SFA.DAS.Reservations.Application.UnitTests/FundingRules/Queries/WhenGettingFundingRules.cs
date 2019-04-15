a091e728a48298a5d958a0d8e6af8bf9e55ce28f
Robin O'Connor <github@rocketmansoftware.com>
2019-04-15 15:49:37 +0100

    Added Get Funding Rules Query

---
+++ src/SFA.DAS.Reservations.Application.UnitTests/FundingRules/Queries/WhenGettingFundingRules.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Application.FundingRules.Queries.GetFundingRules;
+using SFA.DAS.Reservations.Application.FundingRules.Services;
+using SFA.DAS.Reservations.Domain.Rules;
+using SFA.DAS.Reservations.Domain.Rules.Api;
+
+namespace SFA.DAS.Reservations.Application.UnitTests.FundingRules.Queries
+{
+    public class WhenGettingFundingRules
+    {
+        private GetFundingRulesQueryHandler _handler;
+        private Mock<IRulesService> _service;
+        private GetFundingRulesApiResponse _expectedFundingRules;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _expectedFundingRules = new GetFundingRulesApiResponse
+            {
+                CourseRules = new List<ReservationRule>(),
+                GlobalRules = new List<GlobalRule>()
+            };
+            
+            _service = new Mock<IRulesService>();
+            _service.Setup(s => s.GetFundingRules()).ReturnsAsync(_expectedFundingRules);
+
+            _handler = new GetFundingRulesQueryHandler(_service.Object);
+        }
+
+        [Test]
+        public async Task Then_The_Courses_Are_Returned()
+        {
+            //Act
+            var actual = await _handler.Handle(new GetFundingRulesQuery(), new CancellationToken());
+
+            //Assert
+            actual.FundingRules.Should().BeEquivalentTo(_expectedFundingRules);
+        }
+
+        [Test]
+        public void Then_Throws_Exception_If_One_Occurs()
+        {
+            //Arrange
+            var expectedException = new Exception();
+            _service.Setup(s => s.GetFundingRules()).Throws(expectedException);
+
+            //Act
+            var actualException = Assert.ThrowsAsync<Exception>(() => 
+                _handler.Handle(new GetFundingRulesQuery(), new CancellationToken()));
+
+            //Assert
+            Assert.AreEqual(expectedException, actualException);
+        }
+
+    }
+}
---
+++ src/SFA.DAS.Reservations.Application/FundingRules/Queries/GetFundingRules/GetFundingRulesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Reservations.Application.FundingRules.Services;
+
+namespace SFA.DAS.Reservations.Application.FundingRules.Queries.GetFundingRules
+{
+    public class GetFundingRulesQueryHandler : IRequestHandler<GetFundingRulesQuery, GetFundingRulesResult>
+    {
+        private readonly IRulesService _service;
+
+        public GetFundingRulesQueryHandler(IRulesService service)
+        {
+            _service = service;
+        }
+
+        public async Task<GetFundingRulesResult> Handle(GetFundingRulesQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _service.GetFundingRules();
+
+            return new GetFundingRulesResult
+            {
+                FundingRules = response
+            };
+        }
+    }
+}
