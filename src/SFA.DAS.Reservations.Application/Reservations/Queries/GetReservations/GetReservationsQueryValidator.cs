54fe32700f9c5ab1aa3f6a79053b6e392f765b4b
Scott Wakefield <scott@expertcc.co.uk>
2019-04-25 15:50:59 +0100

    MF-82 added validator for get reservations query.

---
+++ src/SFA.DAS.Reservations.Application/Reservations/Queries/GetReservations/GetReservationsQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.Reservations.Application.Validation;
+
+namespace SFA.DAS.Reservations.Application.Reservations.Queries.GetReservations
+{
+    public class GetReservationsQueryValidator : IValidator<GetReservationsQuery>
+    {
+        public Task<ValidationResult> ValidateAsync(GetReservationsQuery query)
+        {
+            var result = new ValidationResult();
+
+            if (query.AccountId < 1)
+            {
+                result.AddError(nameof(query.AccountId));
+            }
+            
+            return Task.FromResult(result);
+        }
+    }
+}
