d91e5c932610668ee3c4f5d7c4afaca6fdbd893b
Scott Wakefield <scott@expertcc.co.uk>
2019-04-29 12:01:34 +0100

    MF-82 added usage of validator.

--- src/SFA.DAS.Reservations.Application/Reservations/Queries/GetReservations/GetReservationsQueryHandler.cs
+++ src/SFA.DAS.Reservations.Application/Reservations/Queries/GetReservations/GetReservationsQueryHandler.cs
@@ -1,30 +1,41 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Options;
+using SFA.DAS.Reservations.Application.Validation;
 using SFA.DAS.Reservations.Domain.Reservations;
 using SFA.DAS.Reservations.Domain.Reservations.Api;
 using SFA.DAS.Reservations.Infrastructure.Api;
 using SFA.DAS.Reservations.Infrastructure.Configuration;
+using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
 
 namespace SFA.DAS.Reservations.Application.Reservations.Queries.GetReservations
 {
     public class GetReservationsQueryHandler : IRequestHandler<GetReservationsQuery, GetReservationsResult>
     {
         private readonly ReservationsApiConfiguration _config;
+        private readonly IValidator<GetReservationsQuery> _validator;
         private readonly IApiClient _apiClient;
 
-        public GetReservationsQueryHandler(IOptions<ReservationsApiConfiguration> configOptions, IApiClient apiClient)
+        public GetReservationsQueryHandler(IValidator<GetReservationsQuery> validator, IOptions<ReservationsApiConfiguration> configOptions, IApiClient apiClient)
         {
             _config = configOptions.Value;
+            _validator = validator;
             _apiClient = apiClient;
         }
 
         public async Task<GetReservationsResult> Handle(GetReservationsQuery request, CancellationToken cancellationToken)
         {
-            //todo: validation - accountid not null and parsable to long
-            
+            var validationResult = await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid())
+            {
+                throw new ValidationException(
+                    new ValidationResult("The following parameters have failed validation", validationResult.ErrorList), null, null);
+            }
+
             var apiReservations = await _apiClient.GetAll<GetReservationResponse>(new ReservationApiRequest(_config.Url, request.AccountId));
             
             var result = new GetReservationsResult
