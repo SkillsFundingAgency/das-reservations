e9608e911f830af0fcd50aed7509d6ad94ce83c4
Akshay Chugh <akshay.chugh30@outlook.com>
2019-05-01 11:48:45 +0100

    Added unit test and back link

--- src/SFA.DAS.Reservations.Web.UnitTests/Employers/WhenViewingTheStartPage.cs
+++ src/SFA.DAS.Reservations.Web.UnitTests/Employers/WhenViewingTheStartPage.cs
@@ -1,19 +1,86 @@
-﻿using FluentAssertions;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using FluentAssertions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
+using SFA.DAS.Reservations.Application.Reservations.Services;
 using SFA.DAS.Reservations.Web.Controllers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+using NLog;
+using NLog.Web.LayoutRenderers;
+using SFA.DAS.Reservations.Application.FundingRules.Queries.GetFundingRules;
+using SFA.DAS.Reservations.Domain.Rules;
+using SFA.DAS.Reservations.Domain.Rules.Api;
 
 namespace SFA.DAS.Reservations.Web.UnitTests.Employers
 {
     public class WhenViewingTheStartPage
     {
-        [Test, MoqAutoData]
-        public void Then_Returns_The_Index_View(
-            EmployerReservationsController controller)
-        {
-            var result = controller.Index() as ViewResult;
+        private EmployerReservationsController _controller;
+        private Mock<IMediator> _mediator;
+        private Mock<IHashingService> _IHashingService;
 
-            result.Should().NotBeNull();
+        [SetUp]
+        public void Arrange()
+        {
+            _mediator = new Mock<IMediator>();
+            _IHashingService = new Mock<IHashingService>();
+            _controller = new EmployerReservationsController(_mediator.Object, _IHashingService.Object);
+            
         }
+
+        
+        [Test]
+        public async Task ThenRedirectToIndexIfNoFundingRulesExist()
+        {
+            //arrange
+            var result = _mediator.Setup(x => x.Send(It.IsAny<GetFundingRulesQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GetFundingRulesResult()
+                    {
+                        FundingRules = new GetFundingRulesApiResponse()
+                        {
+                            GlobalRules = new List<GlobalRule>(),
+                            Rules = new List<ReservationRule>()
+                            
+                        }
+                    });
+
+            //act 
+            var view = await _controller.Index() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(view.ViewName, "Index");
+
+        }
+
+        [Test]
+        public async Task ThenRedirectToFundingPausedIfFundingRulesExist()
+        {
+            //arrange
+            var result = _mediator.Setup(x => x.Send(It.IsAny<GetFundingRulesQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GetFundingRulesResult()
+                {
+                    FundingRules = new GetFundingRulesApiResponse()
+                    {
+                        GlobalRules = new List<GlobalRule>(){new GlobalRule()},
+                        Rules = new List<ReservationRule>()
+
+                    }
+                });
+
+            //act 
+            var view = await _controller.Index() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(view.ViewName, "EmployerFundingPaused");
+
+        }
+
     }
 }
