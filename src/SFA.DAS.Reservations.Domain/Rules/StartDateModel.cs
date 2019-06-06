846df0fb1aece470a4a62c2066f2381624684334
Robin O'Connor <github@rocketmansoftware.com>
2019-06-06 10:05:39 +0100

    Added equality checker to Start Date Model

--- src/SFA.DAS.Reservations.Domain/Rules/StartDateModel.cs
+++ src/SFA.DAS.Reservations.Domain/Rules/StartDateModel.cs
@@ -11,5 +11,22 @@ namespace SFA.DAS.Reservations.Domain.Rules
         {
             return $"{StartDate:MMMM yyyy} to {EndDate:MMMM yyyy}";
         }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is StartDateModel targetModel)) return false;
+
+            return StartDate.Year.Equals(targetModel.StartDate.Year) && 
+                   StartDate.Month.Equals(targetModel.StartDate.Month) && 
+                   StartDate.Day.Equals(targetModel.StartDate.Day) && 
+                   EndDate.Year.Equals(targetModel.EndDate.Year) && 
+                   EndDate.Month.Equals(targetModel.EndDate.Month) && 
+                   EndDate.Day.Equals(targetModel.EndDate.Day);
+        }
+
+        public override int GetHashCode()
+        {
+            return -1;
+        }
     }
 }
---
+++ src/SFA.DAS.Reservations.Web.UnitTests/Models/WhenComparingStartDateModels.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Domain.Rules;
+
+namespace SFA.DAS.Reservations.Web.UnitTests.Models
+{
+    public class WhenComparingStartDateModels
+    {
+        [Test]
+        public void ThenWillReturnSameIfBothDatesAreSetAndEqual()
+        {
+            var source = new StartDateModel {StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1)};
+            var target = new StartDateModel {StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1)};
+
+            Assert.IsTrue(source.Equals(target));
+        }
+
+        [Test]
+        public void ThenWillReturnSameIfStartDateSetAndEqual()
+        {
+            var source = new StartDateModel {StartDate = DateTime.Now};
+            var target = new StartDateModel {StartDate = DateTime.Now};
+
+            Assert.IsTrue(source.Equals(target));
+        }
+
+        [Test]
+        public void ThenWillReturnDifferentIfStartDatesAreSetAndNotEqual()
+        {
+            var source = new StartDateModel {StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1)};
+            var target = new StartDateModel {StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(1)};
+
+            Assert.IsFalse(source.Equals(target));
+        }
+
+        [Test]
+        public void ThenWillReturnDifferentIfEndDatesAreSetAndNotEqual()
+        {
+            var source = new StartDateModel {StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1)};
+            var target = new StartDateModel {StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(2)};
+
+            Assert.IsFalse(source.Equals(target));
+        }
+
+        [Test]
+        public void ThenWillReturnDifferentIfTargetIsNotSameType()
+        {
+            var source = new StartDateModel {StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1)};
+
+            Assert.IsFalse(source.Equals(123));
+        }
+
+        [Test]
+        public void ThenWillReturnSameIfBothAreUnset()
+        {
+            var source = new StartDateModel();
+            var target = new StartDateModel();
+
+            Assert.IsTrue(source.Equals(target));
+        }
+    }
+}
