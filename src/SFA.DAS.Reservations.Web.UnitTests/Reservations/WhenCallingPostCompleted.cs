d6f54b8edd862ac9dcb31289e12bf589a378627e
Scott Wakefield <scott@expertcc.co.uk>
2019-06-12 19:34:17 +0100

    MF-170 consolidated view models, simplified mechanism to get urls.

--- src/SFA.DAS.Reservations.Web.UnitTests/Reservations/WhenCallingPostCompleted.cs
+++ src/SFA.DAS.Reservations.Web.UnitTests/Reservations/WhenCallingPostCompleted.cs
@@ -2,7 +2,11 @@
 using AutoFixture.AutoMoq;
 using AutoFixture.NUnit3;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Moq;
 using NUnit.Framework;
+using SFA.DAS.Reservations.Domain.Interfaces;
+using SFA.DAS.Reservations.Infrastructure.Configuration;
 using SFA.DAS.Reservations.Web.Controllers;
 using SFA.DAS.Reservations.Web.Infrastructure;
 using SFA.DAS.Reservations.Web.Models;
@@ -21,7 +25,7 @@ namespace SFA.DAS.Reservations.Web.UnitTests.Reservations
 
         [Test, AutoData]
         public void And_Has_Ukprn_And_ValidationError_Then_Return_Provider_Completed_View(
-            ConfirmationRedirectViewModel model, 
+            CompletedViewModel model, 
             ReservationsRouteModel routeModel)
         {
             var controller = _fixture.Create<ReservationsController>();
@@ -36,7 +40,7 @@ namespace SFA.DAS.Reservations.Web.UnitTests.Reservations
 
         [Test, AutoData]
         public void And_No_Ukprn_And_ValidationError_Then_Return_Employer_Completed_View(
-            ConfirmationRedirectViewModel model, 
+            CompletedViewModel model, 
             ReservationsRouteModel routeModel)
         {
             routeModel.UkPrn = null;
@@ -50,43 +54,105 @@ namespace SFA.DAS.Reservations.Web.UnitTests.Reservations
             Assert.AreEqual(ViewNames.EmployerCompleted, actualModel.ViewName);
         }
         
-        [TestCase(ConfirmationRedirectViewModel.RedirectOptions.RecruitAnApprentice)]
-        [TestCase(ConfirmationRedirectViewModel.RedirectOptions.AddAnApprentice)]
-        [TestCase(ConfirmationRedirectViewModel.RedirectOptions.Homepage)]
-        [TestCase(ConfirmationRedirectViewModel.RedirectOptions.FindApprenticeshipTraining)]
-        public void Then_The_Request_Is_Redirected_Based_On_The_Selection(string selection)
+        [TestCase(CompletedReservationWhatsNext.RecruitAnApprentice)]
+        [TestCase(CompletedReservationWhatsNext.AddAnApprentice)]
+        [TestCase(CompletedReservationWhatsNext.Homepage)]
+        [TestCase(CompletedReservationWhatsNext.FindApprenticeshipTraining)]
+        public void And_Has_Ukprn_Then_The_Request_Is_Redirected_Based_On_The_Selection(CompletedReservationWhatsNext selection)
         {
-            var model = _fixture.Create<ConfirmationRedirectViewModel>();
-            var routeModel = _fixture.Create<ReservationsRouteModel>();
-            var controller = _fixture.Create<ReservationsController>();
+            var model = _fixture.Create<CompletedViewModel>();
             model.WhatsNext = selection;
+            var routeModel = _fixture.Create<ReservationsRouteModel>();
 
+            var config = _fixture.Freeze<IOptions<ReservationsWebConfiguration>>();
+            var providerRecruitUrl = _fixture.Create<string>();
+            var addApprenticeUrl = _fixture.Create<string>();
+            var mockUrlHelper = _fixture.Freeze<Mock<IExternalUrlHelper>>();
+            mockUrlHelper
+                .Setup(helper => helper.GenerateUrl(routeModel.UkPrn.ToString(), "", "", "recruit", "", ""))
+                .Returns(providerRecruitUrl);
+            mockUrlHelper
+                .Setup(helper => helper.GenerateAddApprenticeUrl(routeModel.UkPrn, routeModel.Id.Value, routeModel.AccountLegalEntityPublicHashedId, model.StartDate, model.CourseId))
+                .Returns(addApprenticeUrl);
+            var controller = _fixture.Create<ReservationsController>();
+            
             var actual = controller.PostCompleted(routeModel, model);
 
             var result = actual as RedirectResult;
             Assert.IsNotNull(result);
             switch (selection)
             {
-                case (ConfirmationRedirectViewModel.RedirectOptions.RecruitAnApprentice):
-                    Assert.AreEqual(model.RecruitApprenticeUrl,result.Url);
+                case (CompletedReservationWhatsNext.RecruitAnApprentice):
+                    Assert.AreEqual(providerRecruitUrl,result.Url);
                     break;
 
-                case (ConfirmationRedirectViewModel.RedirectOptions.FindApprenticeshipTraining):
-                    Assert.AreEqual(model.FindApprenticeshipTrainingUrl,result.Url);
+                case (CompletedReservationWhatsNext.FindApprenticeshipTraining):
+                    Assert.AreEqual(config.Value.FindApprenticeshipTrainingUrl,result.Url);
                     break;
 
-                case (ConfirmationRedirectViewModel.RedirectOptions.AddAnApprentice):
-                    Assert.AreEqual(model.ApprenticeUrl, result.Url);
+                case (CompletedReservationWhatsNext.AddAnApprentice):
+                    Assert.AreEqual(addApprenticeUrl, result.Url);
                     break;
 
-                case (ConfirmationRedirectViewModel.RedirectOptions.Homepage):
-                    Assert.AreEqual(model.DashboardUrl,result.Url);
+                case (CompletedReservationWhatsNext.Homepage):
+                    Assert.AreEqual(config.Value.DashboardUrl,result.Url);
                     break;
 
                 default: 
                     Assert.Fail();
                     break;
+            }
+        }
 
+        [TestCase(CompletedReservationWhatsNext.RecruitAnApprentice)]
+        [TestCase(CompletedReservationWhatsNext.AddAnApprentice)]
+        [TestCase(CompletedReservationWhatsNext.Homepage)]
+        [TestCase(CompletedReservationWhatsNext.FindApprenticeshipTraining)]
+        public void And_No_Ukprn_Then_The_Request_Is_Redirected_Based_On_The_Selection(CompletedReservationWhatsNext selection)
+        {
+            var model = _fixture.Create<CompletedViewModel>();
+            model.WhatsNext = selection;
+            var routeModel = _fixture.Create<ReservationsRouteModel>();
+            routeModel.UkPrn = null;
+
+            var config = _fixture.Freeze<IOptions<ReservationsWebConfiguration>>();
+            var employerRecruitUrl = _fixture.Create<string>();
+            var addApprenticeUrl = _fixture.Create<string>();
+            var mockUrlHelper = _fixture.Freeze<Mock<IExternalUrlHelper>>();
+            mockUrlHelper
+                .Setup(helper =>
+                    helper.GenerateUrl(routeModel.EmployerAccountId, "", "", "recruit", "accounts", ""))
+                .Returns(employerRecruitUrl);
+            mockUrlHelper
+                .Setup(helper => helper.GenerateAddApprenticeUrl(routeModel.UkPrn, routeModel.Id.Value, routeModel.AccountLegalEntityPublicHashedId, model.StartDate, model.CourseId))
+                .Returns(addApprenticeUrl);
+            var controller = _fixture.Create<ReservationsController>();
+            
+            var actual = controller.PostCompleted(routeModel, model);
+
+            var result = actual as RedirectResult;
+            Assert.IsNotNull(result);
+            switch (selection)
+            {
+                case (CompletedReservationWhatsNext.RecruitAnApprentice):
+                    Assert.AreEqual(employerRecruitUrl,result.Url);
+                    break;
+
+                case (CompletedReservationWhatsNext.FindApprenticeshipTraining):
+                    Assert.AreEqual(config.Value.FindApprenticeshipTrainingUrl,result.Url);
+                    break;
+
+                case (CompletedReservationWhatsNext.AddAnApprentice):
+                    Assert.AreEqual(addApprenticeUrl, result.Url);
+                    break;
+
+                case (CompletedReservationWhatsNext.Homepage):
+                    Assert.AreEqual(config.Value.EmployerDashboardUrl,result.Url);
+                    break;
+
+                default: 
+                    Assert.Fail();
+                    break;
             }
         }
     }
--- src/SFA.DAS.Reservations.Web/Models/CompletedViewModel.cs
+++ src/SFA.DAS.Reservations.Web/Models/CompletedViewModel.cs
@@ -1,61 +1,24 @@
 ﻿using System;
-using SFA.DAS.Reservations.Domain.Courses;
-using SFA.DAS.Reservations.Web.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace SFA.DAS.Reservations.Web.Models
 {
-    public class CompletedViewModel : AddApprenticeViewModel
+    public class CompletedViewModel 
     {
-        public CompletedViewModel(
-            Guid reservationId, 
-            DateTime startDate, 
-            DateTime expiryDate, 
-            Course course, 
-            string accountLegalEntityPublicHashedId, 
-            uint? ukPrn = null,
-            string accountLegalEntityName = "", 
-            string dashboardUrl = "", 
-            string apprenticeUrl = "",
-            string employerDashboardUrl = "",
-            string recruitApprenticeUrl = "",
-            string findApprenticeshipTrainingUrl = "")
-            : base(
-                apprenticeUrl, 
-                ukPrn, 
-                reservationId, 
-                accountLegalEntityPublicHashedId, 
-                startDate, 
-                course)
-        {
-            ReservationId = reservationId;
-            StartDate = startDate;
-            ExpiryDate = expiryDate;
-            Course = course;
-            AccountLegalEntityPublicHashedId = accountLegalEntityPublicHashedId;
-            UkPrn = ukPrn;
-            AccountLegalEntityName = accountLegalEntityName;
-            DashboardUrl = dashboardUrl;
-            RecruitApprenticeUrl = recruitApprenticeUrl;
-            EmployerDashboardUrl = employerDashboardUrl;
-            FindApprenticeshipTrainingUrl = findApprenticeshipTrainingUrl;
+        [Required(ErrorMessage = "Select what you would like to do next")]
+        public CompletedReservationWhatsNext? WhatsNext { get; set; }
+        public string TrainingDateDescription { get; set; }
+        public string CourseDescription { get; set; }
+        public string AccountLegalEntityName { get; set; }
+        public DateTime StartDate { get; set; }
+        public string CourseId { get; set; }
+    }
 
-
-            ViewName = ukPrn == null ? ViewNames.EmployerCompleted : ViewNames.ProviderCompleted;
-        }
-
-        public Guid ReservationId { get; }
-        public DateTime StartDate { get; }
-        public DateTime ExpiryDate { get; }
-        public Course Course { get; }
-        public string AccountLegalEntityPublicHashedId { get; }
-        public uint? UkPrn { get; }
-        public string DashboardUrl { get;}
-        public string RecruitApprenticeUrl { get; }
-        public bool ShowDashboardUrl => !string.IsNullOrWhiteSpace(DashboardUrl);
-        public bool ShowApprenticeUrl => !string.IsNullOrWhiteSpace(ApprenticeUrl);
-		public string AccountLegalEntityName { get; }
-        public string ViewName { get; }
-        public string EmployerDashboardUrl { get; }
-        public string FindApprenticeshipTrainingUrl { get; }
+    public enum CompletedReservationWhatsNext
+    {
+        Homepage,
+        RecruitAnApprentice,
+        AddAnApprentice,
+        FindApprenticeshipTraining
     }
 }
