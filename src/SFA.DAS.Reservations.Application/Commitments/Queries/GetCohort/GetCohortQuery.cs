642a7e53e3295a934ebcfab8991059dd45438408
Robin O'Connor <github@rocketmansoftware.com>
2019-07-16 11:10:30 +0100

    Add mediator call to get cohort from service

---
+++ src/SFA.DAS.Reservations.Application/Commitments/Queries/GetCohort/GetCohortQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Reservations.Application.Commitments.Queries.GetCohort
+{
+    public class GetCohortQuery : IRequest<GetCohortResponse>
+    {
+        public long CohortId { get; set; }
+    }
+}
