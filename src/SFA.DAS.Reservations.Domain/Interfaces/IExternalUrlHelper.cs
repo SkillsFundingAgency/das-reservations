04bf16a81576b2e552f9bf67c6af76fb369f9f1b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-07-29 19:26:23 +0100

    Add GenerateAddApprenticeUrl to interface
    Correct return type to be string from object

--- src/SFA.DAS.Reservations.Domain/Interfaces/IExternalUrlHelper.cs
+++ src/SFA.DAS.Reservations.Domain/Interfaces/IExternalUrlHelper.cs
@@ -1,8 +1,11 @@
-﻿namespace SFA.DAS.Reservations.Domain.Interfaces
+﻿using System;
+
+namespace SFA.DAS.Reservations.Domain.Interfaces
 {
     public interface IExternalUrlHelper
     {
         string GenerateUrl(UrlParameters urlParameters);
         string GenerateAddApprenticeUrl(UrlParameters urlParameters);
+        string GenerateAddApprenticeUrl(Guid reservationId, string accountLegalEntityPublicHashedId, string courseId, uint ukPrn, DateTime? startDate, string cohortRef);
     }
 }
