959d4d646a254128c0fc71a3bf3b32da771ae4ea
Akshay Chugh <akshay.chugh30@outlook.com>
2019-09-11 11:14:51 +0100

    MF-295 Added AccountApiHealthCheck

---
+++ src/SFA.DAS.Reservations.Infrastructure/HealthCheck/AccountApiHealthCheck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EAS.Account.Api.Client;
+using SFA.DAS.Http;
+using SFA.DAS.Reservations.Infrastructure.Extensions;
+
+namespace SFA.DAS.Reservations.Infrastructure.HealthCheck
+{
+     public class AccountApiHealthCheck : IHealthCheck
+     {
+        private const string HealthCheckResultDescription = "Account Api check";
+        private readonly IAccountApiClient _apiClient;
+        private readonly ILogger<AccountApiHealthCheck> _logger;
+
+        public AccountApiHealthCheck(
+            IAccountApiClient apiClient,
+            ILogger<AccountApiHealthCheck> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            _logger.LogInformation("Pinging Commitments API");
+
+            try
+            {
+                var timer = Stopwatch.StartNew();
+                await _apiClient.GetStatistics();
+                timer.Stop();
+
+                var durationString = timer.Elapsed.ToHumanReadableString();
+
+                _logger.LogInformation($"Account API ping successful and took {durationString}");
+
+                return HealthCheckResult.Healthy(HealthCheckResultDescription,
+                    new Dictionary<string, object> { { "Duration", durationString } });
+            }
+            catch (RestHttpClientException e)
+            {
+                _logger.LogWarning($"Account API ping failed : [Code: {e.StatusCode}] - {e.ReasonPhrase}");
+                return HealthCheckResult.Unhealthy(HealthCheckResultDescription, e);
+            }
+        }
+    }
+}
