ca6d815c6c1d6c9b6d9d61ebbc12d6a1fd4470ab
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-09-24 14:23:15 +0100

    WIP - Acceptance test project

---
+++ src/SFA.DAS.Reservations.Web.AcceptanceTests/TestServiceProvider.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Reservations.Web;
+using SFA.DAS.Reservations.Web.Controllers;
+
+namespace SFA.DAS.Reservations.Web.AcceptanceTests
+{
+    public class TestServiceProvider : IServiceProvider
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public TestServiceProvider()
+        {
+            var serviceCollection = new ServiceCollection();
+            var hosting
+            var configuration = GenerateConfiguration();
+
+            var startup = new Startup(configuration);
+
+            startup.ConfigureServices(serviceCollection);
+
+            RegisterControllers(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _serviceProvider.GetService(serviceType);
+        }
+
+        private static IConfigurationRoot GenerateConfiguration()
+        {
+            var configSource = new MemoryConfigurationSource
+            {
+                InitialData = new[]
+                {
+                    new KeyValuePair<string, string>("ConfigurationStorageConnectionString", "UseDevelopmentStorage=true;"),
+                    new KeyValuePair<string, string>("ConfigNames", "SFA.DAS.Reservations.Api"),
+                    new KeyValuePair<string, string>("Environment", "DEV"),
+                    new KeyValuePair<string, string>("Version", "1.0"),
+                }
+            };
+
+            var provider = new MemoryConfigurationProvider(configSource);
+
+            return new ConfigurationRoot(new List<IConfigurationProvider> { provider });
+        }
+
+        private static void RegisterControllers(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient(sp =>
+                new ReservationsController(
+                    sp.GetService<ILogger<HomeController>>(),
+                    sp.GetService<IMediator>())
+                {
+                    ControllerContext = GetControllerContext<HomeController>()
+                });
+            
+        }
+
+        private static ControllerContext GetControllerContext<T>() where T : ControllerBase
+        {
+            var controllerName = typeof(T).Name.Replace("Controller", "");
+
+            var descriptor = new ControllerActionDescriptor
+            {
+                ControllerName = controllerName,
+                ControllerTypeInfo = typeof(T).GetTypeInfo()
+            };
+
+            var httpContext = new DefaultHttpContext();
+            var context = new ControllerContext(new ActionContext(httpContext, new RouteData(), descriptor));
+
+            return context;
+        }
+    }
+}
