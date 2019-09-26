8387f4f48c86f2d745844f59ad200e9d89b8e8b9
Akshay Chugh <akshay.chugh30@outlook.com>
2019-09-26 10:05:23 +0100

    MF-281 Created PostSelectCourseViewModel

---
+++ src/SFA.DAS.Reservations.Web/Models/PostSelectCourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Reservations.Web.Models
+{
+    public class PostSelectCourseViewModel
+    {
+        public string SelectedCourseId { get; set; }
+        [Required(ErrorMessage = "Select whether you know which apprenticeship training your apprentice will take")]
+        public bool? ApprenticeTrainingKnown { get; set; }
+    }
+}
