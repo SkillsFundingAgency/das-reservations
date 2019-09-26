a372d46f03a7f3abdd1afccf225c2c32df4c8a12
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-09-26 09:59:30 +0100

    Add acceptance test project and test provider

---
+++ src/SFA.DAS.Reservations.Web.AcceptanceTests/Infrastructure/TestData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SFA.DAS.Reservations.Domain.Courses;
+using SFA.DAS.Reservations.Domain.Employers;
+using SFA.DAS.Reservations.Domain.Rules;
+using SFA.DAS.Reservations.Web.Models;
+
+namespace SFA.DAS.Reservations.Web.AcceptanceTests.Infrastructure
+{
+    public class TestData
+    {
+        public ReservationsRouteModel ReservationRouteModel { get; set; }
+        public AccountLegalEntity AccountLegalEntity { get; set; }
+        public Course Course { get; set; }
+        public TrainingDateModel TrainingDate { get; set; }
+
+        public void BuildTrainingDateModel(string startMonth)
+        {
+            var startDate = DateTime.Parse($"{DateTime.UtcNow.Year} {startMonth} 01");
+            TrainingDate = new TrainingDateModel
+            {
+                StartDate = startDate,
+                EndDate = startDate.AddMonths(2)
+            };
+        }
+    }
+}
