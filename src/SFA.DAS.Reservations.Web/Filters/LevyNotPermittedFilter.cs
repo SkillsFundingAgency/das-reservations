e15b847d0c75df7bff1f45e21470ec8eda5e0a57
Akshay Chugh <akshay.chugh30@outlook.com>
2019-10-09 17:01:07 +0100

    MF-269 Unit tests and implementation

---
+++ src/SFA.DAS.Reservations.Web/Filters/LevyNotPermittedFilter.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SFA.DAS.Encoding;
+using SFA.DAS.Reservations.Application.Employers.Queries.GetLegalEntities;
+using SFA.DAS.Reservations.Web.AppStart;
+using SFA.DAS.Reservations.Web.Infrastructure;
+
+namespace SFA.DAS.Reservations.Web.Filters
+{
+    public class LevyNotPermittedFilter : ActionFilterAttribute
+    {
+        private readonly ServiceParameters _serviceParameters;
+        private readonly IEncodingService _encodingService;
+        private readonly IMediator _mediator;
+
+        public LevyNotPermittedFilter(
+            ServiceParameters serviceParameters,
+            IEncodingService encodingService,
+            IMediator mediator)
+        {
+            _serviceParameters = serviceParameters;
+            _encodingService = encodingService;
+            _mediator = mediator;
+        }
+
+        public override async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next)
+        {
+            if (_serviceParameters.AuthenticationType == AuthenticationType.Employer)
+            {
+                if (!context.RouteData.Values.TryGetValue("employerAccountId", out var employerAccountId))
+                {
+                    context.Result = new RedirectToRouteResult(RouteNames.Error500, null);
+                    return;
+                }
+
+                var decodedAccountId = _encodingService.Decode(employerAccountId.ToString(), EncodingType.AccountId);
+                
+                var legalEntities = await _mediator.Send(new GetLegalEntitiesQuery {AccountId = decodedAccountId});
+
+                if (legalEntities.AccountLegalEntities.Any(x => x.IsLevy))
+                {
+                    context.Result = new RedirectToRouteResult(RouteNames.Error403,null);
+                    return;
+                }
+            }
+
+            await next();
+        }
+    }
+}
