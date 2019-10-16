3696be6d19dd8aa4dcfb9cf0a0d4b9837b3cb2b7
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-10-16 07:58:52 +0100

    Add UkPrn and IsEmptyCohortFromSelect to result
    Updated the GetCachedReservationQueryHandler to return the UkPrn and
    IsEmptyCohortFromSelect

--- src/SFA.DAS.Reservations.Application/Reservations/Queries/GetCachedReservation/GetCachedReservationResult.cs
+++ src/SFA.DAS.Reservations.Application/Reservations/Queries/GetCachedReservation/GetCachedReservationResult.cs
@@ -16,5 +16,7 @@ namespace SFA.DAS.Reservations.Application.Reservations.Queries.GetCachedReserva
         public string AccountName { get; set; }
         public string CohortRef { get; set; }
         public bool EmployerHasSingleLegalEntity  { get; set; }
+        public bool IsEmptyCohortFromSelect { get; set; }
+        public uint? UkPrn { get; set; }
     }
 }
