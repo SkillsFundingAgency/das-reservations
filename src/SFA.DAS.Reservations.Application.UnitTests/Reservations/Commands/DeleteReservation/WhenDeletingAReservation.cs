9a439de5c5dad78908961dea0fa8d686a877b9ca
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-10-24 14:38:13 +0100

    Revert "Revert "Merge pull request #173""
    This reverts commit 7ac1fde0eaf83d8eaa9521c3829ebb5add425c0e.

--- src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Commands/DeleteReservation/WhenDeletingAReservation.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Commands/DeleteReservation/WhenDeletingAReservation.cs
@@ -66,7 +66,7 @@ namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Commands.Delet
             [Frozen] Mock<IOptions<ReservationsApiConfiguration>> mockOptions,
             DeleteReservationCommandHandler handler)
         {
-            var expectedApiRequest = new ReservationApiRequest(mockOptions.Object.Value.Url, command.ReservationId);
+            var expectedApiRequest = new ReservationApiRequest(mockOptions.Object.Value.Url, command.ReservationId, command.DeletedByEmployer);
             mockValidator
                 .Setup(validator => validator.ValidateAsync(command))
                 .ReturnsAsync(new ValidationResult());
