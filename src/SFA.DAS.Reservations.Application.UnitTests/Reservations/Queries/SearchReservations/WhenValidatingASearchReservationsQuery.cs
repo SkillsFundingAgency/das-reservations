13b20a33b2ef63e7060cd997db1fba31a4ddd868
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-11-12 10:19:28 +0000

    Change validator for provider Search
    Changed the validator so it now checks the provider permissions API to
    see if it has accounts it can act on behalf of

--- src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Queries/SearchReservations/WhenValidatingASearchReservationsQuery.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/Reservations/Queries/SearchReservations/WhenValidatingASearchReservationsQuery.cs
@@ -1,21 +1,62 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.Reservations.Application.Reservations.Queries.SearchReservations;
+using SFA.DAS.Reservations.Domain.Employers;
+using SFA.DAS.Reservations.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Reservations.Application.UnitTests.Reservations.Queries.SearchReservations
 {
     public class WhenValidatingASearchReservationsQuery
     {
-        [Test, AutoData]
-        public async Task Then_Is_Valid(
+
+        [Test, MoqAutoData]
+        public async Task Then_Is_Not_Valid_If_No_UkPrn_Is_Set(
             SearchReservationsQuery query,
+            [Frozen] Mock<IProviderPermissionsService> providerPermissionService,
             SearchReservationsQueryValidator validator)
         {
+            query.ProviderId = 0;
+
+            var result = await validator.ValidateAsync(query);
+
+            result.IsValid().Should().BeFalse();
+            result.ErrorList.Should().Contain("ProviderId|ProviderId has not been supplied");
+            providerPermissionService.Verify(x=>x.GetTrustedEmployers(It.IsAny<uint>()), Times.Never);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_Is_Valid_When_Provider_Has_Permissions(
+            SearchReservationsQuery query,
+            [Frozen] Mock<IProviderPermissionsService> providerPermissionService,
+            SearchReservationsQueryValidator validator)
+        {
+            providerPermissionService.Setup(x => x.GetTrustedEmployers(query.ProviderId))
+                .ReturnsAsync(new List<Employer>{new Employer()});
+
             var result = await validator.ValidateAsync(query);
 
             result.IsValid().Should().BeTrue();
+            result.FailedAuthorisationValidation.Should().BeFalse();
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_If_Provider_Has_No_Permission_Value_Is_Set_Then_Failed_Authorisation_Is_Set_To_True(
+            SearchReservationsQuery query,
+            [Frozen] Mock<IProviderPermissionsService> providerPermissionService,
+            SearchReservationsQueryValidator validator)
+        {
+            providerPermissionService.Setup(x => x.GetTrustedEmployers(query.ProviderId))
+                .ReturnsAsync(new List<Employer>());
+
+            var result = await validator.ValidateAsync(query);
+
+            result.IsValid().Should().BeTrue();
+            result.FailedAuthorisationValidation.Should().BeTrue();
         }
     }
 }
