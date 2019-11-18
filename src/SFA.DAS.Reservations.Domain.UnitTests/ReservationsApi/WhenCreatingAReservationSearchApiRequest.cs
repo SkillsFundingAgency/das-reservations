effb7c16a4532451767943d512a6727901a814b3
Scott Wakefield <scott@expertcc.co.uk>
2019-11-18 14:07:57 +0000

    MF-317 adding other filters to api request.

--- src/SFA.DAS.Reservations.Domain.UnitTests/ReservationsApi/WhenCreatingAReservationSearchApiRequest.cs
+++ src/SFA.DAS.Reservations.Domain.UnitTests/ReservationsApi/WhenCreatingAReservationSearchApiRequest.cs
@@ -16,7 +16,104 @@ namespace SFA.DAS.Reservations.Domain.UnitTests.ReservationsApi
         {
             var request = new SearchReservationsApiRequest(url, filter);
 
-            request.SearchUrl.Should().Be($"{url}api/reservations/search?providerId={filter.ProviderId}&searchTerm={filter.Filter.SearchTerm}&pageNumber={filter.Filter.PageNumber}&pageItemCount={filter.Filter.PageSize}&selectedCourse={filter.Filter.SelectedCourse}");
+            request.SearchUrl.Should().Be($"{url}api/reservations/search" +
+                                          $"?providerId={filter.ProviderId}" +
+                                          $"&pageNumber={filter.Filter.PageNumber}" +
+                                          $"&pageItemCount={filter.Filter.PageSize}" +
+                                          $"&searchTerm={filter.Filter.SearchTerm}" +
+                                          $"&selectedEmployer={filter.Filter.SelectedEmployer}" +
+                                          $"&selectedCourse={filter.Filter.SelectedCourse}" +
+                                          $"&selectedStartDate={filter.Filter.SelectedStartDate}");
+        }
+
+        [Test, AutoData]
+        public void And_No_SearchTerm_Then_Not_In_SearchUrl(
+            string url,
+            SearchReservationsRequest filter)
+        {
+            filter.Filter.SearchTerm = null;
+
+            var request = new SearchReservationsApiRequest(url, filter);
+
+            request.SearchUrl.Should().Be($"{url}api/reservations/search" +
+                                          $"?providerId={filter.ProviderId}" +
+                                          $"&pageNumber={filter.Filter.PageNumber}" +
+                                          $"&pageItemCount={filter.Filter.PageSize}" +
+                                          $"&selectedEmployer={filter.Filter.SelectedEmployer}" +
+                                          $"&selectedCourse={filter.Filter.SelectedCourse}" +
+                                          $"&selectedStartDate={filter.Filter.SelectedStartDate}");
+        }
+
+        [Test, AutoData]
+        public void And_No_SelectedEmployer_Then_Not_In_SearchUrl(
+            string url,
+            SearchReservationsRequest filter)
+        {
+            filter.Filter.SelectedEmployer = null;
+
+            var request = new SearchReservationsApiRequest(url, filter);
+
+            request.SearchUrl.Should().Be($"{url}api/reservations/search" +
+                                          $"?providerId={filter.ProviderId}" +
+                                          $"&pageNumber={filter.Filter.PageNumber}" +
+                                          $"&pageItemCount={filter.Filter.PageSize}" +
+                                          $"&searchTerm={filter.Filter.SearchTerm}" +
+                                          $"&selectedCourse={filter.Filter.SelectedCourse}" +
+                                          $"&selectedStartDate={filter.Filter.SelectedStartDate}");
+        }
+
+        [Test, AutoData]
+        public void And_No_SelectedCourse_Then_Not_In_SearchUrl(
+            string url,
+            SearchReservationsRequest filter)
+        {
+            filter.Filter.SelectedCourse = null;
+
+            var request = new SearchReservationsApiRequest(url, filter);
+
+            request.SearchUrl.Should().Be($"{url}api/reservations/search" +
+                                          $"?providerId={filter.ProviderId}" +
+                                          $"&pageNumber={filter.Filter.PageNumber}" +
+                                          $"&pageItemCount={filter.Filter.PageSize}" +
+                                          $"&searchTerm={filter.Filter.SearchTerm}" +
+                                          $"&selectedEmployer={filter.Filter.SelectedEmployer}" +
+                                          $"&selectedStartDate={filter.Filter.SelectedStartDate}");
+        }
+
+        [Test, AutoData]
+        public void And_No_SelectedStartDate_Then_Not_In_SearchUrl(
+            string url,
+            SearchReservationsRequest filter)
+        {
+            filter.Filter.SelectedStartDate = null;
+
+            var request = new SearchReservationsApiRequest(url, filter);
+
+            request.SearchUrl.Should().Be($"{url}api/reservations/search" +
+                                          $"?providerId={filter.ProviderId}" +
+                                          $"&pageNumber={filter.Filter.PageNumber}" +
+                                          $"&pageItemCount={filter.Filter.PageSize}" +
+                                          $"&searchTerm={filter.Filter.SearchTerm}" +
+                                          $"&selectedEmployer={filter.Filter.SelectedEmployer}" +
+                                          $"&selectedCourse={filter.Filter.SelectedCourse}");
+        }
+
+        [Test, AutoData]
+        public void And_No_SearchTerm_Or_Filters_Then_Not_In_SearchUrl(
+            string url,
+            SearchReservationsRequest filter)
+        {
+            filter.Filter.SearchTerm = null;
+            filter.Filter.SelectedEmployer = null;
+            filter.Filter.SelectedCourse = null;
+            filter.Filter.SelectedStartDate = null;
+
+            var request = new SearchReservationsApiRequest(url, filter);
+
+            request.SearchUrl.Should().Be($"{url}api/reservations/search" +
+                                          $"?providerId={filter.ProviderId}" +
+                                          $"&pageNumber={filter.Filter.PageNumber}" +
+                                          $"&pageItemCount={filter.Filter.PageSize}");
         }
     }
 }
--- src/SFA.DAS.Reservations.Domain/Reservations/ReservationFilter.cs
+++ src/SFA.DAS.Reservations.Domain/Reservations/ReservationFilter.cs
@@ -5,6 +5,8 @@
         public string SearchTerm { get; set; }
         public int PageSize { get; set; }
         public int PageNumber { get; set; }
+        public string SelectedEmployer { get; set; }
         public string SelectedCourse { get; set; }
+        public string SelectedStartDate { get; set; }
     }
 }
