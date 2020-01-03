2f55b48d77f4954f4380732b3a82000db4940bc3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-01-03 09:43:58 +0000

    Added in failed agreement check
    If the check fails for auto reservation, it is then checked to make sure that the agreement has been signed. If it has not then an exception is thrown

---
+++ src/SFA.DAS.Reservations.Application/Exceptions/EmployerAgreementNotSignedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.Reservations.Application.Exceptions
+{
+    public class EmployerAgreementNotSignedException : Exception
+    {
+        public long AccountId;
+        public long AccountLegalEntityId;
+        public EmployerAgreementNotSignedException(long accountId, long accountLegalEntityId)
+        {
+            AccountId = accountId;
+            AccountLegalEntityId = accountLegalEntityId;
+        }
+    }
+}
