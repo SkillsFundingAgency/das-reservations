ace877e74fce0c81617e180bdf9b41d14723a7e9
Robin O'Connor <github@rocketmansoftware.co.uk>
2020-02-05 15:23:12 +0000

    Added cookie management code
    This still needs to be manually tested but all unit tests are passing and the main bulk of the code has now been added

---
+++ src/SFA.DAS.Reservations.Web/Extensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace SFA.DAS.Reservations.Web.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool? ToBoolean(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+
+            if (!bool.TryParse(value, out var result))
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
