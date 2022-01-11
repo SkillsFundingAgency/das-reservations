753afb00f969e578e8bb8b3dc7d7644521da9587
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-01-11 14:53:05 +0000

    change single line if

--- src/SFA.DAS.Reservations.Web/Models/GlobalRuleViewModel.cs
+++ src/SFA.DAS.Reservations.Web/Models/GlobalRuleViewModel.cs
@@ -3,20 +3,22 @@ using SFA.DAS.Reservations.Domain.Rules;
 
 namespace SFA.DAS.Reservations.Web.Models
 {
-	public class GlobalRuleViewModel
-	{
+    public class GlobalRuleViewModel
+    {
         public GlobalRuleViewModel(GlobalRule activeGlobalRule)
         {
-			if (activeGlobalRule == null) return;
-            ActiveFrom = activeGlobalRule.ActiveFrom;
-			ActiveTo = activeGlobalRule.ActiveTo;
-			RuleType = activeGlobalRule.RuleType;
-		}
+            if (activeGlobalRule != null)
+            {
+                ActiveFrom = activeGlobalRule.ActiveFrom;
+                ActiveTo = activeGlobalRule.ActiveTo;
+                RuleType = activeGlobalRule.RuleType;
+            }
+        }
 
         public DateTime? ActiveFrom { get; set; }
-		public DateTime? ActiveTo { get; set; }
-		public GlobalRuleType RuleType { get; set; }
+        public DateTime? ActiveTo { get; set; }
+        public GlobalRuleType RuleType { get; set; }
 
-		public string ActiveRuleActiveToDateText => ActiveTo?.ToString("MMMM yyyy");
-	}
+        public string ActiveRuleActiveToDateText => ActiveTo?.ToString("MMMM yyyy");
+    }
 }
