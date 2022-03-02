e680f48a8e7717d608ee0078e125c7d008b90915
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-03-02 14:54:37 +0000

    missed a unit test

--- src/SFA.DAS.Reservations.Web.UnitTests/Reservations/WhenCallingGetApprenticeshipTraining.cs
+++ src/SFA.DAS.Reservations.Web.UnitTests/Reservations/WhenCallingGetApprenticeshipTraining.cs
@@ -311,7 +311,7 @@ namespace SFA.DAS.Reservations.Web.UnitTests.Reservations
                 .ReturnsAsync(getCoursesResult);
             routeModel.FromReview = true;
             mockUrlHelper
-                .Setup(helper => helper.GenerateCohortDetailsUrl(routeModel.UkPrn, routeModel.EmployerAccountId,
+                .Setup(helper => helper.GenerateCohortDetailsUrl(routeModel.UkPrn, string.Empty,
                     cachedReservationResult.CohortRef, false, It.IsAny<string>(), string.Empty))
                 .Returns(cohortDetailsUrl);
 
