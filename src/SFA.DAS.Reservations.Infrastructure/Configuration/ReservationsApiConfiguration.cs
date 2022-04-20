0833ceccc4c8b3e63776fe10f17c0c4c641c10df
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-04-20 16:12:42 +0100

    [CON-4715] Allow MI access token to be obtained when only identifier is supplied

--- src/SFA.DAS.Reservations.Infrastructure/Configuration/ReservationsApiConfiguration.cs
+++ src/SFA.DAS.Reservations.Infrastructure/Configuration/ReservationsApiConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class ReservationsApiConfiguration
     {
-        public virtual string Url { get; set; }
+        public string Url { get; set; }
         public string Id { get; set; }
         public string Secret { get; set; }
         public string Identifier { get; set; }
