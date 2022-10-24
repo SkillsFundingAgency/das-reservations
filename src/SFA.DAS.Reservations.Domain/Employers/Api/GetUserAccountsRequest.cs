df10199b146b84e5b654bf5a1603bded0eba9e5d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-24 21:42:24 +0100

    FAI-626 - Add Request and Response for getting user accounts

---
+++ src/SFA.DAS.Reservations.Domain/Employers/Api/GetUserAccountsRequest.cs
@@ -0,0 +1,21 @@
+using System.Web;
+using SFA.DAS.Reservations.Domain.Interfaces;
+
+namespace SFA.DAS.Reservations.Domain.Employers.Api
+{
+    public class GetUserAccountsRequest: IGetApiRequest
+    {
+        private readonly string _userId;
+        private readonly string _email;
+
+        public GetUserAccountsRequest(string baseUrl, string userId, string email)
+        {
+            BaseUrl = baseUrl;
+            _userId = HttpUtility.UrlEncode(userId);
+            _email = HttpUtility.UrlEncode(email);
+        }
+
+        public string GetUrl => $"{BaseUrl}api/accountusers/{_userId}/accounts?email={_email}";
+        public string BaseUrl { get; }
+    }
+}
