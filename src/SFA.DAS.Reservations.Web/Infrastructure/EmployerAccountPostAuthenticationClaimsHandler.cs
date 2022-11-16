922738fe2bb5031f38b8b5fbbf3ca28d1262ce0c
Chris Woodcock <chris@humesoftware.com>
2022-11-16 15:35:49 +0000

    Using environment specific footer urls

--- src/SFA.DAS.Reservations.Web/Infrastructure/EmployerAccountPostAuthenticationClaimsHandler.cs
+++ src/SFA.DAS.Reservations.Web/Infrastructure/EmployerAccountPostAuthenticationClaimsHandler.cs
@@ -8,40 +8,41 @@ using SFA.DAS.GovUK.Auth.Services;
 using SFA.DAS.Reservations.Domain.Interfaces;
 using SFA.DAS.Reservations.Infrastructure.Configuration;
 
-namespace SFA.DAS.Reservations.Web.Infrastructure;
-
-public class EmployerAccountPostAuthenticationClaimsHandler : ICustomClaims
+namespace SFA.DAS.Reservations.Web.Infrastructure
 {
-    private readonly IEmployerAccountService _employerAccountService;
-    private readonly ReservationsWebConfiguration _configuration;
-
-    public EmployerAccountPostAuthenticationClaimsHandler(IOptions<ReservationsWebConfiguration> configuration, IEmployerAccountService employerAccountService)
+    public class EmployerAccountPostAuthenticationClaimsHandler : ICustomClaims
     {
-        _employerAccountService = employerAccountService;
-        _configuration = configuration.Value;
-    }
-    public async Task<IEnumerable<Claim>> GetClaims(TokenValidatedContext tokenValidatedContext)
-    {
-        string userId;
-        var email = string.Empty;
-        if (_configuration.UseGovSignIn)
-        {
-            userId = tokenValidatedContext.Principal.Claims
-                .First(c => c.Type.Equals(ClaimTypes.NameIdentifier))
-                .Value;
-            email = tokenValidatedContext.Principal.Claims
-                .First(c => c.Type.Equals(ClaimTypes.Email))
-                .Value;
-        }
-        else
-        {
-            userId = tokenValidatedContext.Principal.Claims
-                .First(c => c.Type.Equals(EmployerClaims.IdamsUserIdClaimTypeIdentifier))
-                .Value;
-        }
-            
-        var result = await _employerAccountService.GetClaim(userId, EmployerClaims.AccountsClaimsTypeIdentifier, email);
+        private readonly IEmployerAccountService _employerAccountService;
+        private readonly ReservationsWebConfiguration _configuration;
 
-        return new List<Claim> {result};
+        public EmployerAccountPostAuthenticationClaimsHandler(IOptions<ReservationsWebConfiguration> configuration, IEmployerAccountService employerAccountService)
+        {
+            _employerAccountService = employerAccountService;
+            _configuration = configuration.Value;
+        }
+        public async Task<IEnumerable<Claim>> GetClaims(TokenValidatedContext tokenValidatedContext)
+        {
+            string userId;
+            var email = string.Empty;
+            if (_configuration.UseGovSignIn)
+            {
+                userId = tokenValidatedContext.Principal.Claims
+                    .First(c => c.Type.Equals(ClaimTypes.NameIdentifier))
+                    .Value;
+                email = tokenValidatedContext.Principal.Claims
+                    .First(c => c.Type.Equals(ClaimTypes.Email))
+                    .Value;
+            }
+            else
+            {
+                userId = tokenValidatedContext.Principal.Claims
+                    .First(c => c.Type.Equals(EmployerClaims.IdamsUserIdClaimTypeIdentifier))
+                    .Value;
+            }
+
+            var result = await _employerAccountService.GetClaim(userId, EmployerClaims.AccountsClaimsTypeIdentifier, email);
+
+            return new List<Claim> { result };
+        }
     }
 }
