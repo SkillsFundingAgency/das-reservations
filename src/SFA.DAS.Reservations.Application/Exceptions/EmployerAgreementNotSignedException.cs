2af57a6ba79bfc6ff2b0144d556804dcb57f2935
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-25 13:00:18 +0000

    Sorting out some warnings

--- src/SFA.DAS.Reservations.Application/Exceptions/EmployerAgreementNotSignedException.cs
+++ src/SFA.DAS.Reservations.Application/Exceptions/EmployerAgreementNotSignedException.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 
-namespace SFA.DAS.Reservations.Application.Exceptions
+namespace SFA.DAS.Reservations.Application.Exceptions;
+
+public class EmployerAgreementNotSignedException : Exception
 {
-    public class EmployerAgreementNotSignedException : Exception
+    public long AccountId;
+    public long AccountLegalEntityId;
+        
+    public EmployerAgreementNotSignedException(long accountId, long accountLegalEntityId)
     {
-        public long AccountId;
-        public long AccountLegalEntityId;
-        public EmployerAgreementNotSignedException(long accountId, long accountLegalEntityId)
-        {
-            AccountId = accountId;
-            AccountLegalEntityId = accountLegalEntityId;
-        }
+        AccountId = accountId;
+        AccountLegalEntityId = accountLegalEntityId;
     }
 }
--- src/SFA.DAS.Reservations.Infrastructure/TagHelpers/ExternalUrlHelper.cs
+++ src/SFA.DAS.Reservations.Infrastructure/TagHelpers/ExternalUrlHelper.cs
@@ -5,256 +5,255 @@ using Microsoft.Extensions.Options;
 using SFA.DAS.Reservations.Domain.Interfaces;
 using SFA.DAS.Reservations.Infrastructure.Configuration;
 
-namespace SFA.DAS.Reservations.Infrastructure.TagHelpers
+namespace SFA.DAS.Reservations.Infrastructure.TagHelpers;
+
+public class ExternalUrlHelper : IExternalUrlHelper
 {
-    public class ExternalUrlHelper : IExternalUrlHelper
+    private readonly IConfiguration _configuration;
+    private readonly ReservationsWebConfiguration _options;
+
+    public ExternalUrlHelper(IOptions<ReservationsWebConfiguration> options, IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-        private readonly ReservationsWebConfiguration _options;
+        _configuration = configuration;
+        _options = options.Value;
+    }
 
-        public ExternalUrlHelper(IOptions<ReservationsWebConfiguration> options, IConfiguration configuration)
+    /// <summary>
+    /// usage https://subDomain.baseUrl/folder/id/controller/action?queryString
+    /// </summary>
+    /// <param name="urlParameters"></param>
+    /// <returns></returns>
+    public string GenerateUrl(UrlParameters urlParameters)
+    {
+        var baseUrl = GetBaseUrl();
+
+        return FormatUrl(baseUrl, urlParameters);
+    }
+
+    public string GenerateAddApprenticeUrl(Guid reservationId, string accountLegalEntityPublicHashedId,
+        string courseId, uint? ukPrn, DateTime? startDate, string cohortRef, string accountHashedId,
+        bool isEmptyEmployerCohort = false, string transferSenderId = "", string encodedPledgeApplicationId= "", string journeyData = "")
+    {
+        var queryString = $"?reservationId={reservationId}";
+
+        if (ukPrn.HasValue && !isEmptyEmployerCohort)
         {
-            _configuration = configuration;
-            _options = options.Value;
+            queryString += $"&employerAccountLegalEntityPublicHashedId={accountLegalEntityPublicHashedId}";
+        }
+        else
+        {
+            queryString += $"&accountLegalEntityHashedId={accountLegalEntityPublicHashedId}";
+        }
+        if (ukPrn.HasValue && isEmptyEmployerCohort)
+        {
+            queryString += $"&providerId={ukPrn}";
         }
 
-        /// <summary>
-        /// usage https://subDomain.baseUrl/folder/id/controller/action?queryString
-        /// </summary>
-        /// <param name="urlParameters"></param>
-        /// <returns></returns>
-        public string GenerateUrl(UrlParameters urlParameters)
+        if (startDate.HasValue)
         {
-            var baseUrl = GetBaseUrl();
-
-            return FormatUrl(baseUrl, urlParameters);
+            queryString += $"&startMonthYear={startDate:MMyyyy}";
         }
 
-        public string GenerateAddApprenticeUrl(Guid reservationId, string accountLegalEntityPublicHashedId,
-            string courseId, uint? ukPrn, DateTime? startDate, string cohortRef, string accountHashedId,
-            bool isEmptyEmployerCohort = false, string transferSenderId = "", string encodedPledgeApplicationId= "", string journeyData = "")
+        if (!string.IsNullOrWhiteSpace(courseId))
         {
-            var queryString = $"?reservationId={reservationId}";
+            queryString += $"&courseCode={courseId}";
+        }
+            
+        if (!string.IsNullOrWhiteSpace(journeyData))
+        {
+            queryString += $"&journeyData={journeyData}";
+        }
 
-            if (ukPrn.HasValue && !isEmptyEmployerCohort)
+        var isLevyAccount = string.IsNullOrWhiteSpace(courseId) && !startDate.HasValue;
+
+        if (isLevyAccount)
+        {
+            queryString += "&autocreated=true";
+        }
+
+        if (!string.IsNullOrEmpty(transferSenderId))
+        {
+            queryString += $"&transferSenderId={transferSenderId}";
+        }
+
+        if (!string.IsNullOrEmpty(encodedPledgeApplicationId))
+        {
+            queryString += $"&encodedPledgeApplicationId={encodedPledgeApplicationId}";
+        }
+
+        string controller = "unapproved", action, id;
+            
+        if (ukPrn.HasValue && !isEmptyEmployerCohort)
+        {
+            action = "add/apprentice";
+            id = ukPrn.ToString();
+        }
+        else if (ukPrn.HasValue)
+        {
+            action = "add/apprentice";
+            id = accountHashedId;
+        }
+        else
+        {
+            action = "add";
+            id = accountHashedId;
+        }
+
+        if (!string.IsNullOrEmpty(cohortRef))
+        {
+            controller += $"/{cohortRef}";
+            action = "apprentices/add";
+        }
+
+        var urlParameters = new UrlParameters
+        {
+            Id = id,
+            Controller = controller,
+            Action = action,
+            QueryString = queryString
+        };
+
+        return GenerateAddApprenticeUrl(urlParameters);
+    }
+
+
+    /// <summary>
+    /// usage https://subDomain.baseUrl/folder/id/controller/action?queryString
+    /// </summary>
+    /// <param name="urlParameters"></param>
+    /// <returns></returns>
+    public string GenerateAddApprenticeUrl(UrlParameters urlParameters)
+    {
+        var baseUrl = _configuration["AuthType"].Equals("employer", StringComparison.CurrentCultureIgnoreCase)
+            ? _options.EmployerApprenticeUrl
+            : _options.ApprenticeUrl;
+
+        return FormatUrl(baseUrl, urlParameters);
+    }
+
+    public string GenerateCohortDetailsUrl(uint? ukprn, string accountId, string cohortRef, bool isEmptyCohort = false, 
+        string journeyData = "", string accountLegalEntityHashedId = "")
+    {
+        var queryString = isEmptyCohort && ukprn.HasValue ? $"?providerId={ukprn}" : "";
+
+        if (!string.IsNullOrWhiteSpace(journeyData))
+        {
+            if (string.IsNullOrWhiteSpace(queryString))
             {
-                queryString += $"&employerAccountLegalEntityPublicHashedId={accountLegalEntityPublicHashedId}";
+                queryString = $"?journeyData={journeyData}";
             }
             else
-            {
-                queryString += $"&accountLegalEntityHashedId={accountLegalEntityPublicHashedId}";
-            }
-            if (ukPrn.HasValue && isEmptyEmployerCohort)
-            {
-                queryString += $"&providerId={ukPrn}";
-            }
-
-            if (startDate.HasValue)
-            {
-                queryString += $"&startMonthYear={startDate:MMyyyy}";
-            }
-
-            if (!string.IsNullOrWhiteSpace(courseId))
-            {
-                queryString += $"&courseCode={courseId}";
-            }
-            
-            if (!string.IsNullOrWhiteSpace(journeyData))
             {
                 queryString += $"&journeyData={journeyData}";
             }
+        }
 
-            var isLevyAccount = string.IsNullOrWhiteSpace(courseId) && !startDate.HasValue;
-
-            if (isLevyAccount)
+        if (!string.IsNullOrWhiteSpace(accountLegalEntityHashedId))
+        {
+            if (string.IsNullOrWhiteSpace(queryString))
             {
-                queryString += "&autocreated=true";
-            }
-
-            if (!string.IsNullOrEmpty(transferSenderId))
-            {
-                queryString += $"&transferSenderId={transferSenderId}";
-            }
-
-            if (!string.IsNullOrEmpty(encodedPledgeApplicationId))
-            {
-                queryString += $"&encodedPledgeApplicationId={encodedPledgeApplicationId}";
-            }
-
-            string controller = "unapproved", action, id;
-            
-            if (ukPrn.HasValue && !isEmptyEmployerCohort)
-            {
-                action = "add/apprentice";
-                id = ukPrn.ToString();
-            }
-            else if (ukPrn.HasValue)
-            {
-                action = "add/apprentice";
-                id = accountHashedId;
+                queryString = $"?accountLegalEntityHashedId={accountLegalEntityHashedId}";
             }
             else
             {
-                action = "add";
-                id = accountHashedId;
+                queryString += $"&accountLegalEntityHashedId={accountLegalEntityHashedId}";
             }
-
-            if (!string.IsNullOrEmpty(cohortRef))
-            {
-                controller += $"/{cohortRef}";
-                action = "apprentices/add";
-            }
-
-            var urlParameters = new UrlParameters
-            {
-                Id = id,
-                Controller = controller,
-                Action = action,
-                QueryString = queryString
-            };
-
-            return GenerateAddApprenticeUrl(urlParameters);
         }
 
-
-        /// <summary>
-        /// usage https://subDomain.baseUrl/folder/id/controller/action?queryString
-        /// </summary>
-        /// <param name="urlParameters"></param>
-        /// <returns></returns>
-        public string GenerateAddApprenticeUrl(UrlParameters urlParameters)
+        var urlParameters = new UrlParameters
         {
-            var baseUrl = _configuration["AuthType"].Equals("employer", StringComparison.CurrentCultureIgnoreCase)
-                ? _options.EmployerApprenticeUrl
-                : _options.ApprenticeUrl;
+            Id = ukprn.HasValue && !isEmptyCohort ? ukprn.Value.ToString() : accountId,
+            Controller = "unapproved",
+            Action = isEmptyCohort ? "add/assign" : string.IsNullOrEmpty(accountId) ? $"{cohortRef}/details" : cohortRef,
+            Folder = "",
+            QueryString = queryString
+        };
 
-            return FormatUrl(baseUrl, urlParameters);
-        }
+        return GenerateAddApprenticeUrl(urlParameters);
+    }
 
-        public string GenerateCohortDetailsUrl(uint? ukprn, string accountId, string cohortRef, bool isEmptyCohort = false, 
-            string journeyData = "", string accountLegalEntityHashedId = "")
+    public string GenerateConfirmEmployerUrl(uint ukprn, string employerAccountLegalEntityPublicHashedId)
+    {
+        var queryString = $"?employerAccountLegalEntityPublicHashedId={employerAccountLegalEntityPublicHashedId}";
+
+        var urlParameters = new UrlParameters
         {
-            var queryString = isEmptyCohort && ukprn.HasValue ? $"?providerId={ukprn}" : "";
+            Id = ukprn.ToString(),
+            Controller = "unapproved/add",
+            Action = "confirm-employer",
+            Folder = "",
+            QueryString = queryString
+        };
 
-            if (!string.IsNullOrWhiteSpace(journeyData))
-            {
-                if (string.IsNullOrWhiteSpace(queryString))
-                {
-                    queryString = $"?journeyData={journeyData}";
-                }
-                else
-                {
-                    queryString += $"&journeyData={journeyData}";
-                }
-            }
+        return FormatUrl(_options.ApprenticeUrl, urlParameters);
+    }
 
-            if (!string.IsNullOrWhiteSpace(accountLegalEntityHashedId))
-            {
-                if (string.IsNullOrWhiteSpace(queryString))
-                {
-                    queryString = $"?accountLegalEntityHashedId={accountLegalEntityHashedId}";
-                }
-                else
-                {
-                    queryString += $"&accountLegalEntityHashedId={accountLegalEntityHashedId}";
-                }
-            }
+    private static string FormatUrl(string baseUrl, UrlParameters urlParameters)
+    {
+        var urlString = new StringBuilder();
 
-            var urlParameters = new UrlParameters
-            {
-                Id = ukprn.HasValue && !isEmptyCohort ? ukprn.Value.ToString() : accountId,
-                Controller = "unapproved",
-                Action = isEmptyCohort ? "add/assign" : string.IsNullOrEmpty(accountId) ? $"{cohortRef}/details" : cohortRef,
-                Folder = "",
-                QueryString = queryString
-            };
+        urlString.Append(FormatBaseUrl(baseUrl, urlParameters.SubDomain, urlParameters.Folder));
 
-            return GenerateAddApprenticeUrl(urlParameters);
-        }
-
-        public string GenerateConfirmEmployerUrl(uint ukprn, string employerAccountLegalEntityPublicHashedId)
+        if (!string.IsNullOrEmpty(urlParameters.Id))
         {
-            var queryString = $"?employerAccountLegalEntityPublicHashedId={employerAccountLegalEntityPublicHashedId}";
-
-            var urlParameters = new UrlParameters
-            {
-                Id = ukprn.ToString(),
-                Controller = "unapproved/add",
-                Action = "confirm-employer",
-                Folder = "",
-                QueryString = queryString
-            };
-
-            return FormatUrl(_options.ApprenticeUrl, urlParameters);
+            urlString.Append($"{urlParameters.Id}/");
         }
 
-        private static string FormatUrl(string baseUrl, UrlParameters urlParameters)
+        if (!string.IsNullOrEmpty(urlParameters.Controller))
         {
-            var urlString = new StringBuilder();
-
-            urlString.Append(FormatBaseUrl(baseUrl, urlParameters.SubDomain, urlParameters.Folder));
-
-            if (!string.IsNullOrEmpty(urlParameters.Id))
-            {
-                urlString.Append($"{urlParameters.Id}/");
-            }
-
-            if (!string.IsNullOrEmpty(urlParameters.Controller))
-            {
-                urlString.Append($"{urlParameters.Controller}/");
-            }
-
-            if (!string.IsNullOrEmpty(urlParameters.Action))
-            {
-                urlString.Append($"{urlParameters.Action}/");
-            }
-
-            if (!string.IsNullOrEmpty(urlParameters.QueryString))
-            {
-                return $"{urlString.ToString().TrimEnd('/')}{urlParameters.QueryString}";
-            }
-
-            return urlString.ToString().TrimEnd('/');
+            urlString.Append($"{urlParameters.Controller}/");
         }
 
-
-        private static string FormatBaseUrl(string url, string subDomain = "", string folder = "")
+        if (!string.IsNullOrEmpty(urlParameters.Action))
         {
-            var returnUrl = url.EndsWith("/")
-                ? url
-                : url + "/";
-
-            if (!string.IsNullOrEmpty(subDomain))
-            {
-                returnUrl = returnUrl.Replace("https://", $"https://{subDomain}.");
-            }
-
-            if (!string.IsNullOrEmpty(folder))
-            {
-                returnUrl = $"{returnUrl}{folder}/";
-            }
-
-            return returnUrl;
+            urlString.Append($"{urlParameters.Action}/");
         }
 
-        private string GetBaseUrl()
+        if (!string.IsNullOrEmpty(urlParameters.QueryString))
         {
-            return _configuration["AuthType"].Equals("employer", StringComparison.CurrentCultureIgnoreCase)
-                ? _options.EmployerDashboardUrl
-                : _options.DashboardUrl;
+            return $"{urlString.ToString().TrimEnd('/')}{urlParameters.QueryString}";
         }
 
-        public string GenerateDashboardUrl(string accountId = null)
+        return urlString.ToString().TrimEnd('/');
+    }
+
+
+    private static string FormatBaseUrl(string url, string subDomain = "", string folder = "")
+    {
+        var returnUrl = url.EndsWith("/")
+            ? url
+            : url + "/";
+
+        if (!string.IsNullOrEmpty(subDomain))
         {
-            return string.IsNullOrEmpty(accountId)
-                ? GenerateUrl(new UrlParameters {Controller = "Account"})
-                : GenerateUrl(new UrlParameters
-                {
-                    Controller = "teams",
-                    SubDomain = "accounts",
-                    Folder = "accounts",
-                    Id = accountId
-                });
+            returnUrl = returnUrl.Replace("https://", $"https://{subDomain}.");
         }
+
+        if (!string.IsNullOrEmpty(folder))
+        {
+            returnUrl = $"{returnUrl}{folder}/";
+        }
+
+        return returnUrl;
+    }
+
+    private string GetBaseUrl()
+    {
+        return _configuration["AuthType"].Equals("employer", StringComparison.CurrentCultureIgnoreCase)
+            ? _options.EmployerDashboardUrl
+            : _options.DashboardUrl;
+    }
+
+    public string GenerateDashboardUrl(string accountId = null)
+    {
+        return string.IsNullOrEmpty(accountId)
+            ? GenerateUrl(new UrlParameters {Controller = "Account"})
+            : GenerateUrl(new UrlParameters
+            {
+                Controller = "teams",
+                SubDomain = "accounts",
+                Folder = "accounts",
+                Id = accountId
+            });
     }
 }
