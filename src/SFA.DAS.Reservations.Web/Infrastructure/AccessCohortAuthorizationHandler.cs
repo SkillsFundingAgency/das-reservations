5c0e9eb551eb353fd05eb08b19f33c06c3544fa7
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-28 12:24:43 +0000

    WIP

--- src/SFA.DAS.Reservations.Web/Infrastructure/AccessCohortAuthorizationHandler.cs
+++ src/SFA.DAS.Reservations.Web/Infrastructure/AccessCohortAuthorizationHandler.cs
@@ -52,11 +52,11 @@ public class AccessCohortAuthorizationHandler(ILogger<AccessCohortAuthorizationH
             return false;
         }
         
-        var trustedAccountClaim = context.User.FindFirst(c => c.Type.Equals(ProviderClaims.TrustedEmployerAccounts))?.Value;
-
         logger.LogInformation("AccessCohortAuthorizationHandler.IsAuthorisedToAccessCohort() claims: {claims}",
             JsonConvert.SerializeObject(context.User.Claims.ToDictionary(claim => claim.Type, claim => claim.Value))
         );
+        
+        var trustedAccountClaim = context.User.FindFirst(c => c.Type.Equals(ProviderClaims.TrustedEmployerAccounts))?.Value;
 
         return false;
 
