d9d5bc3b6bbf9f4ed7ac962ae1bfdc9b3b8a686e
Jon Harris <jon.harris@digital.education.gov.uk>
2024-04-04 08:38:27 +0100

    Removing logging

--- src/SFA.DAS.Reservations.Web/Infrastructure/ProviderAuthorizationHandler.cs
+++ src/SFA.DAS.Reservations.Web/Infrastructure/ProviderAuthorizationHandler.cs
@@ -1,13 +1,10 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace SFA.DAS.Reservations.Web.Infrastructure;
 
-public class ProviderAuthorizationHandler(ILogger<ProviderAuthorizationHandler> logger) : AuthorizationHandler<ProviderUkPrnRequirement>, IProviderAuthorisationHandler
+public class ProviderAuthorizationHandler : AuthorizationHandler<ProviderUkPrnRequirement>, IProviderAuthorisationHandler
 {
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ProviderUkPrnRequirement requirement)
     {
@@ -23,10 +20,6 @@ public class ProviderAuthorizationHandler(ILogger<ProviderAuthorizationHandler> 
 
     public bool IsProviderAuthorised(AuthorizationHandlerContext context)
     {
-        logger.LogInformation("ProviderAuthorizationHandler.IsProviderAuthorised() claims: {claims}",
-            JsonConvert.SerializeObject(context.User.Claims.ToDictionary(claim => claim.Type, claim => claim.Value))
-        );
-            
         if (!(context.Resource is HttpContext providerContext && providerContext.Request.RouteValues.ContainsKey(RouteValues.UkPrn)))
             return false;
 
