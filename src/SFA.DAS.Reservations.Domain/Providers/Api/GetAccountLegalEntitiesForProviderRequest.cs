d7430fb7b28aa12fc843f58cbaaed1282fda94b0
Deven Shah <devenshah1974@gmail.com>
2024-06-21 16:27:58 +0100

    CSP-1367 Removed PR client packages and refactored to use outer client

--- src/SFA.DAS.Reservations.Domain/Providers/Api/GetAccountLegalEntitiesForProviderRequest.cs
+++ src/SFA.DAS.Reservations.Domain/Providers/Api/GetAccountLegalEntitiesForProviderRequest.cs
@@ -5,14 +5,14 @@ namespace SFA.DAS.Reservations.Domain.Providers.Api;
 public class GetAccountLegalEntitiesForProviderRequest : IGetApiRequest
 {
     private readonly long _ukprn;
-    
+
     public string GetUrl => $"{BaseUrl}provideraccounts/{_ukprn}/legalentities-with-create-cohort";
     public string BaseUrl { get; }
 
     public GetAccountLegalEntitiesForProviderRequest(string apiBaseUrl, long ukprn)
     {
         _ukprn = ukprn;
-        
+
         BaseUrl = apiBaseUrl.EndsWith('/') ? apiBaseUrl : apiBaseUrl + "/";
     }
 }
