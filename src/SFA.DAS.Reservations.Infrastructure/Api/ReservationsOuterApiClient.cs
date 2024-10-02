2ec8a8560f2e248aaa001eaf6ccf4e36c17f7ee0
Jon Harris <jon.harris@fuitsu.com>
2024-10-02 11:26:00 +0100

    Removed unused interface implementation.

--- src/SFA.DAS.Reservations.Infrastructure/Api/ReservationsOuterApiClient.cs
+++ src/SFA.DAS.Reservations.Infrastructure/Api/ReservationsOuterApiClient.cs
@@ -1,78 +1,51 @@
-using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Polly;
-using Polly.Retry;
 using SFA.DAS.Reservations.Domain.Interfaces;
 using SFA.DAS.Reservations.Infrastructure.Configuration;
 
 namespace SFA.DAS.Reservations.Infrastructure.Api;
 
-public class ReservationsOuterApiClient : IReservationsOuterApiClient
+public class ReservationsOuterApiClient(
+    HttpClient httpClient, 
+    ReservationsOuterApiConfiguration config,
+    ILogger<ReservationsOuterApiClient> logger)
+    : IReservationsOuterApiClient
 {
-    private readonly HttpClient _httpClient;
-    private readonly AsyncRetryPolicy _asyncRetryPolicy;
-    private readonly ReservationsOuterApiConfiguration _config;
-    private readonly ILogger<ReservationsOuterApiClient> _logger;
-
-    public ReservationsOuterApiClient (HttpClient httpClient, ReservationsOuterApiConfiguration config, ILogger<ReservationsOuterApiClient> logger)
+    public async Task<TResponse> Get<TResponse>(IGetApiRequest request)
     {
-        _config = config;
-        _logger = logger;
-        _httpClient = httpClient;
-        _asyncRetryPolicy = GetRetryPolicy();
-    }
-
-    public async Task<TResponse> Get<TResponse>(IGetApiRequest request) 
-    {
-        _logger.LogInformation("Calling Outer API base");
+        logger.LogInformation("Calling Outer API base");
 
         using var httpMessage = new HttpRequestMessage(HttpMethod.Get, request.GetUrl);
 
         AddHeaders(httpMessage);
 
-        var response = await _httpClient.SendAsync(httpMessage).ConfigureAwait(false);
+        var response = await httpClient.SendAsync(httpMessage).ConfigureAwait(false);
 
         if (response.StatusCode.Equals(HttpStatusCode.NotFound))
         {
-            _logger.LogInformation("Found nothing");
+            logger.LogInformation("Found nothing");
             return default;
         }
 
         if (response.IsSuccessStatusCode)
         {
-            _logger.LogInformation("Returned a response");
+            logger.LogInformation("Returned a response");
             var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<TResponse>(json);    
+            return JsonConvert.DeserializeObject<TResponse>(json);
         }
 
-        _logger.LogInformation("Returned a response {StatusCode}",response.StatusCode);
+        logger.LogInformation("Returned a response {StatusCode}", response.StatusCode);
         response.EnsureSuccessStatusCode();
-            
-        return default;
-    }
 
-    public async Task<TResponse> GetWithRetry<TResponse>(IGetApiRequest request)
-    {
-        return await _asyncRetryPolicy.ExecuteAsync(async() => await Get<TResponse>(request));
+        return default;
     }
 
     private void AddHeaders(HttpRequestMessage httpRequestMessage)
     {
-        httpRequestMessage.Headers.Add("Ocp-Apim-Subscription-Key", _config.SubscriptionKey);
+        httpRequestMessage.Headers.Add("Ocp-Apim-Subscription-Key", config.SubscriptionKey);
         httpRequestMessage.Headers.Add("X-Version", "1");
-    }
-
-    private static AsyncRetryPolicy GetRetryPolicy()
-    {
-        const int maxRetryAttempts = 3;
-        var pauseBetweenFailures = TimeSpan.FromSeconds(2);
-
-        return Policy
-            .Handle<HttpRequestException>()
-            .WaitAndRetryAsync(maxRetryAttempts, i => pauseBetweenFailures);
     }
 }
